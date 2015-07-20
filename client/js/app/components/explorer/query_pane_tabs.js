a1eeafae7d33faeaca06024ad7ad97e7d4441fdb
aroc <eric@ericanderson.ca>
2015-07-20 18:22:01 -0400

    ids for tracking purposes.

--- client/js/app/components/explorer/query_pane_tabs.js
+++ client/js/app/components/explorer/query_pane_tabs.js
@@ -14,13 +14,13 @@ var QueryPaneTabs = React.createClass({
     return (
       <ul className="query-pane-tabs nav nav-tabs">
         <li role="presentation" className={this.props.activePane === 'build' ? 'active' : ''}>
-          <a ref="build-tab" href="#" onClick={this.toggled.bind(this, 'build')}>
+          <a ref="build-tab" href="#" id="build-query" onClick={this.toggled.bind(this, 'build')}>
             <span className="icon glyphicon glyphicon-tasks margin-right-tiny"></span>
             Build Query
           </a>
         </li>
         <li role="presentation" className={this.props.activePane === 'browse' ? 'active' : ''}>
-          <a ref="browse-tab" href="#" onClick={this.toggled.bind(this, 'browse')}>
+          <a ref="browse-tab" href="#" id="browse-favs" onClick={this.toggled.bind(this, 'browse')}>
             <span className="icon glyphicon glyphicon-heart margin-right-tiny fav-icon"></span>
             Browse Favorites
           </a>
