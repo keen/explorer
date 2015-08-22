bc8654361ea26464399485aa3919a63915f13f31
aroc <eric@ericanderson.ca>
2015-08-21 18:14:13 -0600

    Better styling for the query builder buttons.

--- client/js/app/components/explorer/query_builder/builder_buttons.js
+++ client/js/app/components/explorer/query_builder/builder_buttons.js
@@ -5,6 +5,7 @@
 var React = require('react');
 var classNames = require('classnames');
 var _ = require('lodash');
+var ExplorerUtils = require('../../../utils/ExplorerUtils');
 
 var BuilderButtons = React.createClass({
 
@@ -45,21 +46,23 @@ var BuilderButtons = React.createClass({
       'disabled': this.props.model.loading,
       'btn btn-primary run-query': true
     });
-    if (!_.isEqual(_.omit(this.props.model, 'originalModel'), this.props.model.originalModel)) {
+    if (ExplorerUtils.isPersisted(this.props.model) && !_.isEqual(this.props.model.query, this.props.model.originalModel.query)) {
       revertBtn = (
-          <button className="pull-left btn btn-default" onClick={this.props.handleRevertChanges}>Revert to original</button>
+        <button className="pull-left btn btn-default margin-right-tiny" onClick={this.props.handleRevertChanges}>Revert to original</button>
       );
     }
     return (
-      <div className="query-builder-buttons row">
-        <div className="col-md-4">
-          {revertBtn}
-        </div>
+      <div className="query-builder-buttons row margin-top-small">
         <div className="col-md-8">
-          <div className="pull-right">
+          <div className="pull-left">
+            {revertBtn}
             <button type="reset" ref="clearquery" className="btn btn-default pull-left" onClick={this.props.clearQuery} id="clear-explorer-query">
               Clear
             </button>
+          </div>
+        </div>
+        <div className="col-md-4">
+          <div className="pull-right">
             <button type="submit" className={queryButtonClasses} onClick={this.props.handleQuerySubmit} ref="runquery" id="run-query">
               <span className="icon glyphicon glyphicon-check"></span>
               {this.queryButtonText()}
