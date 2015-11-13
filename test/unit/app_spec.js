39ab4096a50f17ed196080e48fd38612cabad29a
Joe Wegner <joe@wegnerdesign.com>
2015-11-13 10:21:01 -0600

    run queries automatically on pageload

--- test/unit/app_spec.js
+++ test/unit/app_spec.js
@@ -1,11 +1,35 @@
 var assert = require('chai').assert;
 var expect = require('chai').expect;
 var App = require('../../client/js/app/app.js');
+var QueryStringUtils =  require('../../client/js/app/utils/QueryStringUtils.js');
 var React = require('react/addons');
 var TestUtils = React.addons.TestUtils;
 var TestHelpers = require('../support/TestHelpers.js');
 var sinon = require('sinon');
 
 describe('app', function() {
-  
+  it('runs a query if the querystring has query attrs in it', function () {
+    var queryAttributesStub = sinon.stub(QueryStringUtils, 'getQueryAttributes').returns({
+      query: {
+        event_collection: 'pageview',
+        analysis_type: 'count',
+        time: {
+          relativity: 'this',
+          amount: 14,
+          sub_timeframe: 'days' 
+        }
+      }
+    }); 
+    var xhrOpenStub = sinon.stub(XMLHttpRequest.prototype, 'open');
+    var xhrSendStub = sinon.stub(XMLHttpRequest.prototype, 'send');
+    var client = TestHelpers.createClient();
+
+    App({client: client, targetId: 'explorer'});
+
+    assert.isTrue(queryAttributesStub.calledOnce);
+
+    QueryStringUtils.getQueryAttributes.restore();
+    xhrOpenStub.restore();
+    xhrSendStub.restore();
+  });   
 });
