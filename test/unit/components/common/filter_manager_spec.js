d1b2534e513781698fa84d7113a5a9660e0234d2
aroc <eric@ericanderson.ca>
2015-11-19 16:43:56 -0700

    Fixed FilterManagerSpec

--- test/unit/components/common/filter_manager_spec.js
+++ test/unit/components/common/filter_manager_spec.js
@@ -6,48 +6,41 @@ var React = require('react/addons');
 var TestUtils = React.addons.TestUtils;
 var TestHelpers = require('../../../support/TestHelpers.js');
 var FilterManager = require('../../../../client/js/app/components/common/filter_manager.js');
-var ExplorerActions = require('../../../../client/js/app/actions/ExplorerActions');
 
 describe('components/common/filter_manager', function() {
 
-  before(function () {
-    this.addFilterStub = sinon.stub(ExplorerActions, 'addFilter');
-  });
-
-  after(function () {
-    ExplorerActions.addFilter.restore();
-  });
-
   beforeEach(function() {
-    this.client = TestHelpers.createClient();
-    this.project = TestHelpers.createProject();
-    this.project.loading = true;
-    this.model = TestHelpers.createExplorerModel();
-    this.component = TestUtils.renderIntoDocument(<FilterManager client={this.client} project={this.project} model={this.model} />);
+    this.handleChangeStub = sinon.stub();
+    this.removeFilterStub = sinon.stub();
+    this.addFilterStub = sinon.stub();
+    this.getPropertyType = function(){ return 'String'; };
+
+    var defaultProps = {
+      eventCollection: null,
+      filters: [],
+      handleChange: this.handleChangeStub,
+      removeFilter: this.removeFilterStub,
+      addFilter: this.addFilterStub,
+      getPropertyType: this.getPropertyType,
+      propertyNames: []
+    };
+    this.renderComponent = function(props) {
+      var props = _.assign({}, defaultProps, props);
+      return TestUtils.renderIntoDocument(<FilterManager {...props} />);
+    }
+    this.component = this.renderComponent();
   });
 
-  describe('setup', function(){
-    it('should have tried to create a default empty filter', function () {
-      assert.isTrue(this.addFilterStub.calledOnce);
-    });
-    it('has an empty filters message by default', function(){
-      var message = 'Please select an Event Collection before making a filter.';
-      assert.equal(message, TestUtils.findRenderedDOMComponentWithClass(this.component, 'no-filters-msg').getDOMNode().textContent);
-    });
+  it('has a message telling the user to choose an event collection is one is not set', function(){
+    var message = 'Please select an Event Collection before making a filter.';
+    assert.equal(message, TestUtils.findRenderedDOMComponentWithClass(this.component, 'no-filters-msg').getDOMNode().textContent);
   });
-  describe('field change reactions', function () {
-    describe('event_collection', function () {
-      beforeEach(function(){
-        this.model.query.event_collection = 'click';
-        this.component.forceUpdate();
-      });
-
-      it("no longer shows the 'Please select an event collection' message", function() {
-        assert.lengthOf(TestUtils.scryRenderedDOMComponentsWithClass(this.component, 'no-filters-msg'), 0);
-      });
-      it("has an add filter button", function() {
-        assert.lengthOf(TestUtils.scryRenderedDOMComponentsWithClass(this.component, 'add-filter'), 1);
-      });
-    });
+  it("no longer shows the message when an event collection is set", function() {
+    this.component = this.renderComponent({ eventCollection: 'click' });
+    assert.lengthOf(TestUtils.scryRenderedDOMComponentsWithClass(this.component, 'no-filters-msg'), 0);
+  });
+  it("has an add filter button when an event collection is set", function() {
+    this.component = this.renderComponent({ eventCollection: 'click' });
+    assert.lengthOf(TestUtils.scryRenderedDOMComponentsWithClass(this.component, 'add-filter'), 1);
   });
 });
