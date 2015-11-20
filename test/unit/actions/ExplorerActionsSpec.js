fc97bc4a2edb50aa3e588c65084c939be471fd20
aroc <eric@ericanderson.ca>
2015-11-20 11:06:14 -0700

    Fixed tests.

--- test/unit/actions/ExplorerActionsSpec.js
+++ test/unit/actions/ExplorerActionsSpec.js
@@ -282,7 +282,7 @@ describe('actions/ExplorerActions', function() {
     it('should call the dispatcher to update with the right arguments', function () {
       var expectedUpdates = _.cloneDeep(this.explorer);
       expectedUpdates.loading = false;
-      expectedUpdates.result = 100;
+      expectedUpdates.response = this.response;
       expectedUpdates.metadata.visualization.chart_type = 'metric';
       
       ExplorerActions.execSuccess(this.explorer, this.response);
