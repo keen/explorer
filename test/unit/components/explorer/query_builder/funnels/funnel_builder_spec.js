6df60d3747d973fb94ca0d23af4447f88a1207f7
aroc <eric@ericanderson.ca>
2015-11-21 02:48:15 -0700

    Skeleton of tests for Funnel components.

---
+++ test/unit/components/explorer/query_builder/funnels/funnel_builder_spec.js
@@ -0,0 +1,31 @@
+var _ = require('lodash');
+var assert = require('chai').assert;
+var React = require('react/addons');
+var $R = require('rquery')(_, React);
+var TestUtils = React.addons.TestUtils;
+var TestHelpers = require('../../../../../test/support/TestHelpers');
+var FunnelBuilder = require('../../../../../client/js/app/components/explorer/query_builder/funnels/funnel_builder.js');
+
+describe('components/explorer/query_builder/funnels/funnel_builder', function() {
+    
+  xit('should render as many FunnelStep child components as there are steps', function () {
+    
+  });
+
+  xit('should call ExplorerActions.addStep when the add step link is clicked', function () {
+    
+  });
+
+  xit('should toggle a step active if it is inactive and the header is clicked', function () {
+    
+  });
+
+  xit('should toggle a step inactive if it is active and the header is clicked', function () {
+    
+  });
+
+  xit('should toggle all other steps inactive if a header is clicked that for an inactive step', function () {
+    
+  });
+
+});
---
+++ test/unit/components/explorer/query_builder/funnels/funnel_step_spec.js
@@ -0,0 +1,23 @@
+var _ = require('lodash');
+var assert = require('chai').assert;
+var React = require('react/addons');
+var $R = require('rquery')(_, React);
+var TestUtils = React.addons.TestUtils;
+var TestHelpers = require('../../../../../test/support/TestHelpers');
+var FunnelStep = require('../../../../../client/js/app/components/explorer/query_builder/funnels/funnel_step.js');
+
+describe('components/explorer/query_builder/funnels/funnel_step', function() {
+
+  xit('should ONLY call moveStepUp if the up button is clicked', function () {
+    
+  });
+
+  xit('should ONLY call moveStepDown if the down button is clicked', function () {
+    
+  });
+
+  xit('should ONLY call toggleStepActive if the header is clicked', function () {
+    
+  });
+  
+});
