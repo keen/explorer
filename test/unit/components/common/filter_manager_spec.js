fc68d5d3b341804ecddddf8e4fdecccd8f9607c0
aroc <eric@ericanderson.ca>
2015-11-25 22:13:16 +0100

    Fixes bug where filter removal action gets called without the filter index.

--- test/unit/components/common/filter_manager_spec.js
+++ test/unit/components/common/filter_manager_spec.js
@@ -1,46 +1,53 @@
-/** @jsx React.DOM */
 var sinon = require('sinon');
 var assert = require('chai').assert;
 var _ = require('lodash');
 var React = require('react/addons');
+var $R = require('rquery')(_, React);
 var TestUtils = React.addons.TestUtils;
 var TestHelpers = require('../../../support/TestHelpers.js');
 var FilterManager = require('../../../../client/js/app/components/common/filter_manager.js');
 
+function defaultProps() {
+  return {
+    eventCollection: null,
+    filters: [],
+    handleChange: sinon.stub(),
+    removeFilter: sinon.stub(),
+    addFilter: sinon.stub(),
+    getPropertyType: sinon.stub().returns('String'),
+    propertyNames: []
+  };
+}
+
 describe('components/common/filter_manager', function() {
-
-  beforeEach(function() {
-    this.handleChangeStub = sinon.stub();
-    this.removeFilterStub = sinon.stub();
-    this.addFilterStub = sinon.stub();
-    this.getPropertyType = function(){ return 'String'; };
-
-    var defaultProps = {
-      eventCollection: null,
-      filters: [],
-      handleChange: this.handleChangeStub,
-      removeFilter: this.removeFilterStub,
-      addFilter: this.addFilterStub,
-      getPropertyType: this.getPropertyType,
-      propertyNames: []
-    };
-    this.renderComponent = function(props) {
-      var props = _.assign({}, defaultProps, props);
-      return TestUtils.renderIntoDocument(<FilterManager {...props} />);
-    }
-    this.component = this.renderComponent();
-  });
-
   it('has a message telling the user to choose an event collection is one is not set', function(){
+    var component = TestHelpers.renderComponent(FilterManager, defaultProps);
     var message = 'Please select an Event Collection before making a filter.';
-    assert.equal(message, TestUtils.findRenderedDOMComponentWithClass(this.component, 'no-filters-msg').getDOMNode().textContent);
+    assert.equal(message, TestUtils.findRenderedDOMComponentWithClass(component, 'no-filters-msg').getDOMNode().textContent);
   });
   it("no longer shows the message when an event collection is set", function() {
-    this.component = this.renderComponent({ eventCollection: 'click' });
-    assert.lengthOf(TestUtils.scryRenderedDOMComponentsWithClass(this.component, 'no-filters-msg'), 0);
+    var component = TestHelpers.renderComponent(FilterManager, _.assign(defaultProps(), {
+      eventCollection: 'click'
+    }));
+    assert.lengthOf(TestUtils.scryRenderedDOMComponentsWithClass(component, 'no-filters-msg'), 0);
   });
   it("has an add filter button when an event collection is set", function() {
-    this.component = this.renderComponent({ eventCollection: 'click' });
-    assert.lengthOf(TestUtils.scryRenderedDOMComponentsWithClass(this.component, 'add-filter'), 1);
+    var component = TestHelpers.renderComponent(FilterManager, _.assign(defaultProps(), {
+      eventCollection: 'click'
+    }));
+    assert.lengthOf(TestUtils.scryRenderedDOMComponentsWithClass(component, 'add-filter'), 1);
+  });
+  it('should call removeFilter with the correct filter index when the remove button is clicked', function () {
+    var component = TestHelpers.renderComponent(FilterManager, _.assign(defaultProps(), {
+      eventCollection: 'click',
+      filters: [
+        TestHelpers.createFilter(),
+        TestHelpers.createFilter(),
+        TestHelpers.createFilter()
+      ]
+    }));
+    var node = $R(component).find('.remove-filter').components[1];
+    TestUtils.Simulate.click(node);
+    assert.isTrue(component.props.removeFilter.calledWith(1));
   });
 });
