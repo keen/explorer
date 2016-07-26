171d2a243bddd57cca32aaca313b9243a22fd6c0
Sara Falkoff <sara@keen.io>
2016-07-26 15:18:37 -0700

    Remove optional host parameter from embeddable code sample

--- client/js/app/utils/ExplorerUtils.js
+++ client/js/app/utils/ExplorerUtils.js
@@ -276,7 +276,6 @@ module.exports = {
 
     var defaultKeenJsOpts = {
           requestType: 'jsonp',
-          host: 'api.keen.io/3.0',
           protocol: 'https',
         },
         params = module.exports.queryJSON(explorer),
@@ -284,12 +283,12 @@ module.exports = {
         dynamicCriteria,
         paramNames,
         dynamicConstructorNames = [
-          'host', 'protocol', 'requestType'
+        'protocol', 'requestType'
         ],
         funnelRootParams = [
           'event_collection', 'steps'
         ],
-        dynamicContructorValues;
+        dynamicConstructorValues;
 
     switch(params.analysis_type) {
       case 'funnel':
@@ -305,7 +304,7 @@ module.exports = {
       params.steps = _.map(params.steps, function(step) { return _.omit(step, 'active'); });
     }
 
-    dynamicContructorValues = mapSkip(dynamicConstructorNames, function(name) {
+    dynamicConstructorValues = mapSkip(dynamicConstructorNames, function(name) {
       if (client.config[name] == defaultKeenJsOpts[name]) {
         return SKIP
       }
@@ -324,8 +323,8 @@ module.exports = {
     value = [
       'var client = new Keen({',
       '  projectId: ' + s(client.config.projectId) + ',',
-      '  readKey: ' + s(client.config.readKey) + echoIf(dynamicContructorValues, ','),
-      dynamicContructorValues,
+      '  readKey: ' + s(client.config.readKey) + echoIf(dynamicConstructorValues, ','),
+      dynamicConstructorValues,
       '});',
       '',
       'Keen.ready(function(){',
