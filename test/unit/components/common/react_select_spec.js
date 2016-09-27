6b86d8bb6e65f246e4de3db91bbc68f412a12900
Jack Littleton <jack@keen.io>
2016-09-27 13:37:53 -0700

    Added additional select component unit tests:
    * Test search term containg closing reg exp characters
    * Test search term containing both opening and closing reg exp characters

--- test/unit/components/common/react_select_spec.js
+++ test/unit/components/common/react_select_spec.js
@@ -36,6 +36,20 @@ describe('components/common/react_select', function() {
   });
 
   describe('search', function () {
+    var searchTest = function(selectOptions, searchValue, expectedListItems) {
+      var propsStep1 = _.assign({}, this.component.props, { items: selectOptions, value: searchValue });
+      this.component = TestHelpers.renderComponent(ReactSelect, propsStep1);
+      this.component.setState({ initialFocus: false });
+
+      TestUtils.Simulate.focus(this.component.refs.input);
+      TestUtils.Simulate.change(this.component.refs.input);
+
+      var listItems = _.map(ReactDOM.findDOMNode(this.component.refs.list).childNodes, function(item) {
+        return item.textContent;
+      });
+      assert.sameMembers(listItems, expectedListItems);
+    };
+
     it('searches for items case insensitive', function () {
       var propsStep1 = _.assign({}, this.component.props, { items: ['ONE', 'TWO', 'THREE'], value: 'one' });
       this.component = TestHelpers.renderComponent(ReactSelect, propsStep1);
@@ -50,46 +64,76 @@ describe('components/common/react_select', function() {
       assert.sameMembers(listItems, ['ONE']);
     });
 
-    it('should display items with square bracket (RegExp reserved char)', function() {
-      var propsStep1 = _.assign({}, this.component.props, { items: ['ONE', 'TWO[2]', 'THREE'], value: 'two[' });
-      this.component = TestHelpers.renderComponent(ReactSelect, propsStep1);
-      this.component.setState({ initialFocus: false });
+    it('should display items with opening square bracket (RegExp reserved char)', function() {
+      var selectOptions = ['ONE', 'TWO[2]', 'THREE'];
+      var searchValue = 'two[';
+      var expectedListItems = ['TWO[2]'];
 
-      TestUtils.Simulate.focus(this.component.refs.input);
-      TestUtils.Simulate.change(this.component.refs.input);
-
-      var listItems = _.map(ReactDOM.findDOMNode(this.component.refs.list).childNodes, function(item) {
-        return item.textContent;
-      });
-      assert.sameMembers(listItems, ['TWO[2]']);
+      searchTest(selectOptions, searchValue, expectedListItems);
     });
 
-    it('should display items with curly braces (RegExp reserved char)', function() {
-      var propsStep1 = _.assign({}, this.component.props, { items: ['ONE', 'TWO', 'THREE{tres}'], value: 'three{' });
-      this.component = TestHelpers.renderComponent(ReactSelect, propsStep1);
-      this.component.setState({ initialFocus: false });
+    it('should display items with opening curly brace (RegExp reserved char)', function() {
+      var selectOptions = ['ONE', 'TWO', 'THREE{tres}'];
+      var searchValue = 'three{';
+      var expectedListItems = ['THREE{tres}'];
 
-      TestUtils.Simulate.focus(this.component.refs.input);
-      TestUtils.Simulate.change(this.component.refs.input);
-
-      var listItems = _.map(ReactDOM.findDOMNode(this.component.refs.list).childNodes, function(item) {
-        return item.textContent;
-      });
-      assert.sameMembers(listItems, ['THREE{tres}']);
+      searchTest(selectOptions, searchValue, expectedListItems);
     });
 
-    it('should display items with curly braces (RegExp reserved char)', function() {
-      var propsStep1 = _.assign({}, this.component.props, { items: ['ONE', 'TWO', 'THREE(3)'], value: 'three(' });
-      this.component = TestHelpers.renderComponent(ReactSelect, propsStep1);
-      this.component.setState({ initialFocus: false });
+    it('should display items with opening parenthesis (RegExp reserved char)', function() {
+      var selectOptions = ['ONE', 'TWO', 'THREE(3)'];
+      var searchValue = 'three(';
+      var expectedListItems = ['THREE(3)'];
 
-      TestUtils.Simulate.focus(this.component.refs.input);
-      TestUtils.Simulate.change(this.component.refs.input);
+      searchTest(selectOptions, searchValue, expectedListItems);
+    });
 
-      var listItems = _.map(ReactDOM.findDOMNode(this.component.refs.list).childNodes, function(item) {
-        return item.textContent;
-      });
-      assert.sameMembers(listItems, ['THREE(3)']);
+    it('should display items with closing square bracket (RegExp reserved char)', function() {
+      var selectOptions = ['ONE', 'TWO]xxx[', 'THREE'];
+      var searchValue = 'two]';
+      var expectedListItems = ['TWO]xxx['];
+
+      searchTest(selectOptions, searchValue, expectedListItems);
+    });
+
+    it('should display items with closing curly brace (RegExp reserved char)', function() {
+      var selectOptions = ['ONE', 'TWO', 'THREE}tres'];
+      var searchValue = 'three}';
+      var expectedListItems = ['THREE}tres'];
+
+      searchTest(selectOptions, searchValue, expectedListItems);
+    });
+
+    it('should display items with closing parenthesis (RegExp reserved char)', function() {
+      var selectOptions = ['ONE', 'TWO', 'THREE)yyy'];
+      var searchValue = 'three)';
+      var expectedListItems = ['THREE)yyy'];
+
+      searchTest(selectOptions, searchValue, expectedListItems);
+    });
+
+    it('should display items with both square brackets (RegExp reserved char)', function() {
+      var selectOptions = ['ONE', 'TWO[2]', 'THREE'];
+      var searchValue = 'two[2]';
+      var expectedListItems = ['TWO[2]'];
+
+      searchTest(selectOptions, searchValue, expectedListItems);
+    });
+
+    it('should display items with both curly braces (RegExp reserved char)', function() {
+      var selectOptions = ['ONE', 'TWO', 'THREE{tres}'];
+      var searchValue = 'three{tres}';
+      var expectedListItems = ['THREE{tres}'];
+
+      searchTest(selectOptions, searchValue, expectedListItems);
+    });
+
+    it('should display items with both parentheses (RegExp reserved char)', function() {
+      var selectOptions = ['ONE', 'TWO', 'THREE(yyy)'];
+      var searchValue = 'three(yyy)';
+      var expectedListItems = ['THREE(yyy)'];
+
+      searchTest(selectOptions, searchValue, expectedListItems);
     });
   });
 });
