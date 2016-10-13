7bb36b028fd0d8100328babf674128dcdf5f7471
Jack Littleton <jack@keen.io>
2016-10-13 15:11:33 -0700

    WIP: Adding dataviz config component test

---
+++ test/unit/components/explorer/visualization/keen_viz_config_spec.js
@@ -0,0 +1,20 @@
+var KeenVizConfig = require('../../../../../client/js/app/components/explorer/visualization/keen_viz_config.js');
+var assert = require('chai').assert;
+var React = require('react');
+var TestUtils = require('react-addons-test-utils');
+var TestHelpers = require('../../../../support/TestHelpers');
+
+describe('components/explorer/visualization/keen_vis_config', function() {
+
+  beforeEach(function() {
+    this.component = TestUtils.renderIntoDocument(<KeenVizConfig />);
+  });
+
+  describe('setup', function() {
+    it('is of the right type', function() {
+      assert.isTrue(TestUtils.isCompositeComponentWithType(this.component, KeenVizConfig));
+    });
+
+  });
+
+});
