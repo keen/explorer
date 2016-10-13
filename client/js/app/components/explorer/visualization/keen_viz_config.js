2d2416a2fb0dec88908a98dafd82d7966f40a7b5
Jack Littleton <jack@keen.io>
2016-10-13 11:51:43 -0700

    WIP: Adding dataviz config component

---
+++ client/js/app/components/explorer/visualization/keen_viz_config.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var KeenVizConfig = React.createClass({
+
+  render: function() {
+    return (
+      <div></div>
+    );
+  }
+
+});
+
+module.exports = KeenVizConfig;
