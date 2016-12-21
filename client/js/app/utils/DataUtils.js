b3ee6bed4d42cf946438c061b99043e05237e717
dustinlarimer <dustinlarimer@gmail.com>
2016-12-20 16:46:39 -0800

    Fix string concat to replace commas (breaks CSV format)

--- client/js/app/utils/DataUtils.js
+++ client/js/app/utils/DataUtils.js
@@ -1,20 +1,24 @@
 module.exports = {
   exportToCsv: function(data, filename) {
     var csvContent = 'data:text/csv;charset=utf-8,';
+    var htmlElement;
 
-    data.forEach(function(infoArray, index){
-      dataString = infoArray.join(',');
-      csvContent += index < data.length ? dataString+ '\n' : dataString;
+    data.forEach(function(row, i){
+      row.forEach(function(cell, j){
+        csvContent += String(cell).replace(/,/g, '');
+        if (row.length > j+1) {
+          csvContent += ',';
+        }
+      });
+      if (data.length > i+1) {
+        csvContent += '\n';
+      }
     });
 
-    var encodedUri = encodeURI(csvContent);
-    var link = document.createElement('a');
-
-    link.setAttribute('href', encodedUri);
-    link.setAttribute('download', filename);
-
-    document.body.appendChild(link);
-
-    link.click();
+    htmlElement = document.createElement('a');
+    htmlElement.setAttribute('href', encodeURI(csvContent));
+    htmlElement.setAttribute('download', filename);
+    document.body.appendChild(htmlElement);
+    htmlElement.click();
   }
 }
