cf6d38d9ea3e436c34f92997772ebbf4bc786364
Eric Anderson <eric@ericanderson.ca>
2017-06-14 14:47:50 -0600

    latest build files

--- dist/keen-explorer.js
+++ dist/keen-explorer.js
@@ -139,7 +139,7 @@
 	App.prototype.fetch = function () {
 	  if (this.config.persistence) {
 	    ExplorerActions.fetchAllPersisted(this.config.persistence, function (err) {
-	      if (err) throw new Error('There was an error fetching the persisted explorers: ' + err.text);
+	      if (err) console.error('There was an error fetching the persisted explorers: ', err.stack);
 	    });
 	    // Is this a saved query we want to load?
 	    if (this.config.params.saved_query) {
@@ -163,7 +163,7 @@
 	  if (!arguments.length) return this.config.persistence;
 	  if (typeof bool === 'boolean' && bool) {
 	    if (!this.config.client || !this.config.client.masterKey()) {
-	      throw new Error('This feature requires a client instance with a masterKey value');
+	      console.error('This feature requires a client instance with a masterKey value');
 	    }
 	    this.config.persistence = new Keen.Explorer.Persistence.KeenSavedQueries({
 	      baseUrl: this.config.client.url('queries', 'saved'),
@@ -58619,6 +58619,7 @@
 	  },
 
 	  convertDateToUTC: function convertDateToUTC(date) {
+	    if (typeof date === 'string') date = new Date(date);
 	    return new Date(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(), date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds());
 	  },
 
@@ -62040,7 +62041,7 @@
 
 	  fetchProjectCollections: function fetchProjectCollections(client) {
 	    var project = ProjectStore.getProject();
-	    if (!project) throw new Error("Cannot fetchProjectCollections: No project model has been created yet.");
+	    if (!project) console.error("Cannot fetchProjectCollections: No project model has been created yet.");
 
 	    return client.get(client.url('projectId')).auth(client.masterKey()).send().then(function (res) {
 	      var schema = _.assign({}, project.schema);
@@ -62058,7 +62059,7 @@
 	        loading: false
 	      });
 	    }).catch(function (err) {
-	      throw new Error('Error fetching project collections: ' + err);
+	      console.error('Error fetching project collections: ', err.stack);
 	    });
 	  },
 
@@ -62077,7 +62078,7 @@
 	        loading: false
 	      });
 	    }).catch(function (err) {
-	      throw new Error('Error fetching project collections: ' + err);
+	      console.error('Error fetching project collections: ', err.stack);
 	    });
 	  },
 
@@ -62109,7 +62110,7 @@
 	        });
 	      },
 	      error: function error(err) {
-	        throw new Error("Error requesting latest events for event collection: " + eventCollection);
+	        console.error("Error requesting latest events for event collection: " + eventCollection, err.stack);
 	      },
 	      complete: function complete() {
 	        ProjectActions.updateEventCollection(eventCollection, {
