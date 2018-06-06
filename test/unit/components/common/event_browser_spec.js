e4858ef1db610bb8d4d7ca417ca7706df301aea4
Adam Kasprowicz <adam.kasprowicz@keen.io>
2018-06-06 15:13:58 +0200

    fix test for event browser

--- test/unit/components/common/event_browser_spec.js
+++ test/unit/components/common/event_browser_spec.js
@@ -1,98 +1,103 @@
-let sinon from 'sinon/pkg/sinon.js');
-
-var _ from 'lodash');
-var KeenAnalysis from 'keen-analysis');
+import _ from 'lodash';
+import KeenAnalysis from 'keen-analysis';
 import React from 'react';
-var ReactDOM from 'react-dom');
-var TestUtils from 'react-addons-test-utils');
-var TestHelpers from '../../../support/TestHelpers');
-var EventBrowser from '../../../../lib/js/app/components/common/event_browser.js');
-var Loader from '../../../../lib/js/app/components/common/loader.js');
-var ExplorerActions from '../../../../lib/js/app/actions/ExplorerActions');
-var ExplorerUtils from '../../../../lib/js/app/utils/ExplorerUtils');
-var ProjectUtils from '../../../../lib/js/app/utils/ProjectUtils');
-var FormatUtils from '../../../../lib/js/app/utils/FormatUtils');
-var ProjectActions from '../../../../lib/js/app/actions/ProjectActions');
-var $R from 'rquery')(_, React, ReactDOM, TestUtils);
+import ReactDOM from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
+import TestHelpers from '../../../support/TestHelpers';
+import EventBrowser from '../../../../lib/js/app/components/common/event_browser.js';
+import Loader from '../../../../lib/js/app/components/common/loader.js';
+import ExplorerActions from '../../../../lib/js/app/actions/ExplorerActions';
+import ExplorerUtils from '../../../../lib/js/app/utils/ExplorerUtils';
+import ProjectUtils from '../../../../lib/js/app/utils/ProjectUtils';
+import FormatUtils from '../../../../lib/js/app/utils/FormatUtils';
+import ProjectActions from '../../../../lib/js/app/actions/ProjectActions';
+import rquery from 'rquery';
+const $R = rquery(_, React, ReactDOM, TestUtils);
 
 describe('components/common/event_browser', () => {
+  let runQueryStub;
+  let fetchCollectionSchemaStub;
+  let project;
+  let renderComponent;
+  let client;
+  let component;
+  let currentEventCollection = 'click';
 
-  before(() => {
-    this.runQueryStub = sinon.stub(ExplorerUtils, 'runQuery');
-    sinon.stub(ProjectActions, 'fetchCollectionSchema');
+  beforeAll(() => {
+    runQueryStub = jest.spyOn(ExplorerUtils, 'runQuery').mockImplementation(() => {});
+    fetchCollectionSchemaStub = jest.spyOn(ProjectActions, 'fetchCollectionSchema');
   });
 
-  after(() => {
-    ExplorerUtils.runQuery.restore();
-    ProjectActions.fetchCollectionSchema.restore();
+  afterAll(() => {
+    runQueryStub.mockRestore();
+    fetchCollectionSchemaStub.mockRestore();
   });
 
   beforeEach(() => {
-    this.runQueryStub.reset();
+    runQueryStub.mockClear();
 
-    this.project = TestHelpers.createProject();
-    this.project.loading = true;
-    this.project.client = this.client = new KeenAnalysis(TestHelpers.createClient());
-    this.project.client.resources({
+    project = TestHelpers.createProject();
+    project.loading = true;
+    project.client = client = new KeenAnalysis(TestHelpers.createClient());
+    project.client.resources({
       'events': '{protocol}://{host}/3.0/projects/{projectId}/events'
     });
     // Ensure there are already some recent events
-    this.project.schema.click.recentEvents = [{ name: 'first recent event' }];
+    project.schema.click.recentEvents = [{ name: 'first recent event' }];
 
-    this.renderComponent = function(props) {
-      var props = props || {};
-      var defaultProps = {
-        currentEventCollection: 'click',
-        project: this.project
+    renderComponent = (props = {}) => {
+      const defaultProps = {
+        currentEventCollection,
+        project
       };
       return TestUtils.renderIntoDocument(<EventBrowser {..._.assign({}, defaultProps, props)} />);
     };
-    this.component = this.renderComponent();
+    component = renderComponent();
   });
 
   describe('setup', () => {
     it('is of the right type', () => {
-      assert.isTrue(TestUtils.isCompositeComponentWithType(this.component, EventBrowser));
+      expect(TestUtils.isCompositeComponentWithType(component, EventBrowser)).toBe(true);
     });
 
     it('has a single a Loader component', () => {
-      assert.lengthOf(TestUtils.scryRenderedComponentsWithType(this.component, Loader), 1);
+      expect(TestUtils.scryRenderedComponentsWithType(component, Loader).length).toBe(1);
     });
   });
 
   describe('handling content when event collections exist', () => {
     it('shows UI to browse event collections when they exist', () => {
-      assert.lengthOf($R(this.component).find('.event-browser').components, 1);
+      expect($R(component).find('.event-browser').components.length).toBe(1);
     });
 
     it('does not show UI alert when no event collections exist', () => {
-      assert.lengthOf($R(this.component).find('.no-collections-alert').components, 0);
+      expect($R(component).find('.no-collections-alert').components.length).toBe(0);
     });
   });
 
   describe('handling content when event collections do not exist', () => {
     beforeEach(() => {
-      this.project.eventCollections = [];
-      this.component = TestUtils.renderIntoDocument(<EventBrowser client={this.client} currentEventCollection={this.currentEventCollection} project={this.project} />);
+      project.eventCollections = [];
+      component = TestUtils.renderIntoDocument(<EventBrowser client={client} currentEventCollection={currentEventCollection} project={project} />);
     });
 
     it('does not show UI to browse event collections when they exist', () => {
-      assert.lengthOf($R(this.component).find('.event-browser').components, 0);
+      expect($R(component).find('.event-browser').components.length).toBe(0);
     });
 
     it('shows UI alert when no event collections exist', () => {
-      assert.lengthOf($R(this.component).find('.no-collections-alert').components, 1);
+      expect($R(component).find('.no-collections-alert').components.length).toBe(1);
     });
   });
 
   describe('handling the active event collection', () => {
     it('does not try to access recent events when activeEventCollection is a string that does not match a collection', () => {
-      this.component = this.renderComponent({ currentEventCollection: 'some string that does not match an event collection' });
-      assert.strictEqual(this.component.getRecentEvents(), "");
+      component = renderComponent({ currentEventCollection: 'some string that does not match an event collection' });
+      expect(component.getRecentEvents()).toBe("");
     });
     it('properly returns the recent events when activeEventCollection is a string that matches a collection', () => {
-      this.component = this.renderComponent({ currentEventCollection: 'click' });
-      assert.strictEqual(this.component.getRecentEvents(), FormatUtils.prettyPrintJSON([{ name: 'first recent event' }]));
+      component = renderComponent({ currentEventCollection: 'click' });
+      expect(component.getRecentEvents()).toBe(FormatUtils.prettyPrintJSON([{ name: 'first recent event' }]));
     });
   });
 });
