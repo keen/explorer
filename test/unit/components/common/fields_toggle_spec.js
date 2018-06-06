aac45aad127cd8d4b3318079b0ffe97f4f1fc4a9
Adam Kasprowicz <adam.kasprowicz@keen.io>
2018-06-06 15:30:04 +0200

    fix test fields toggle

--- test/unit/components/common/fields_toggle_spec.js
+++ test/unit/components/common/fields_toggle_spec.js
@@ -1,111 +1,98 @@
-let sinon from 'sinon/pkg/sinon.js');
-
-var _ from 'lodash');
+import _ from 'lodash';
 import React from 'react';
-var ReactDOM from 'react-dom');
-var TestUtils from 'react-addons-test-utils');
-var FieldsToggle from '../../../../lib/js/app/components/common/fields_toggle.js');
-var TestHelpers from '../../../support/TestHelpers');
+import ReactDOM from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
+import FieldsToggle from '../../../../lib/js/app/components/common/fields_toggle.js';
+import TestHelpers from '../../../support/TestHelpers';
 
 describe('components/common/fields_toggle', () => {
+  let getFn;
+  let updateFn;
+  let component;
+  let model;
 
-  before(() => {
-    this.getFn = sinon.stub();
-    this.updateFn = sinon.stub();
+  beforeAll(() => {
+    getFn = jest.fn();
+    updateFn = jest.fn();
   });
 
   beforeEach(() => {
-    this.getFn.reset();
-    this.updateFn.reset();
-    this.model = TestHelpers.createExplorerModel();
-    this.component = null;
+    getFn.mockClear();
+    updateFn.mockClear();
+    model = TestHelpers.createExplorerModel();
+    component = null;
   });
 
   describe('setup', () => {
     beforeEach(() => {
-      this.component = TestUtils.renderIntoDocument(<FieldsToggle model={this.model}
+      component = TestUtils.renderIntoDocument(<FieldsToggle model={model}
                                                                   name="Some Field"
                                                                   initialOpenState={true}
                                                                   attrsToStore={'analysis_type'}
-                                                                  getFn={this.getFn} />);
+                                                                  getFn={getFn} />);
     });
     it('displays the name prop', () => {
-      var nameDisplayed = this.component.refs['name'].textContent;
-      assert.strictEqual(nameDisplayed, 'Some Field');
+      const nameDisplayed = component.refs['name'].textContent;
+      expect(nameDisplayed).toBe('Some Field');
     });
   });
 
   describe('with fieldsCount prop', () => {
     beforeEach(() => {
-      this.component = TestUtils.renderIntoDocument(<FieldsToggle model={this.model}
+      component = TestUtils.renderIntoDocument(<FieldsToggle model={model}
                                                                   name="Some Field"
                                                                   initialOpenState={true}
                                                                   fieldsCount={5}
-                                                                  getFn={this.getFn} />);
+                                                                  getFn={getFn} />);
     });
     it('displays the fields count prop', () => {
-      var nameDisplayed = this.component.refs['icon'].textContent;
-      assert.strictEqual(nameDisplayed, '5');
+      const nameDisplayed = component.refs['icon'].textContent;
+      expect(nameDisplayed).toBe('5');
     });
     it('doesnt call handleFieldsToggle when toggled', () => {
-      var spy = sinon.spy(this.component, 'handleFieldsToggle');
-      TestUtils.Simulate.click(this.component.refs['toggle-label']);
-      assert.isFalse(spy.called);
-      this.component.handleFieldsToggle.restore();
+      const spy = jest.spyOn(component, 'handleFieldsToggle');
+      TestUtils.Simulate.click(component.refs['toggle-label']);
+      expect(spy).not.toHaveBeenCalled();
+      spy.mockRestore();
     });
   });
 
   describe('basic interactions', () => {
     it('calls the toggle function when the toggle-label is clicked', () => {
-      this.component = TestUtils.renderIntoDocument(<FieldsToggle model={this.model}
+      component = TestUtils.renderIntoDocument(<FieldsToggle model={model}
                                                                   name="Some Field"
                                                                   initialOpenState={true}
                                                                   attrsToStore={'analysisType'}
-                                                                  updateFn={this.updateFn}
-                                                                  getFn={this.getFn} />);
-      var spy = sinon.spy(this.component, 'toggle');
-      TestUtils.Simulate.click(this.component.refs['toggle-label']);
-      assert.isTrue(spy.calledOnce);
-      this.component.toggle.restore();
-    });
-
-    it('calls the toggle callback when the toggle-label is clicked', () => {
-      var spy = sinon.spy();
-      this.component = TestUtils.renderIntoDocument(<FieldsToggle model={this.model}
-                                                                  name="Some Field"
-                                                                  initialOpenState={true}
-                                                                  attrsToStore={'analysisType'}
-                                                                  toggleCallback={spy}
-                                                                  getFn={this.getFn}
-                                                                  updateFn={this.updateFn} />);
-
-
-      TestUtils.Simulate.click(this.component.refs['toggle-label']);
-      assert.isTrue(spy.calledOnce);
+                                                                  updateFn={updateFn}
+                                                                  getFn={getFn} />);
+      const spy = jest.spyOn(component, 'toggle');
+      TestUtils.Simulate.click(component.refs['toggle-label']);
+      expect(spy).toHaveBeenCalledTimes(1);
+      spy.mockRestore();
     });
 
     describe('toggles the open class when the toggle-label is clicked', () => {
       it('opening', () => {
-        this.component = TestUtils.renderIntoDocument(<FieldsToggle model={this.model}
+        component = TestUtils.renderIntoDocument(<FieldsToggle model={model}
                                                                     name="Some Field"
                                                                     initialOpenState={true}
                                                                     attrsToStore={'analysisType'}
-                                                                    getFn={this.getFn}
-                                                                    updateFn={this.updateFn} />);
+                                                                    getFn={getFn}
+                                                                    updateFn={updateFn} />);
 
-        TestUtils.Simulate.click(this.component.refs['toggle-label']);
-        assert.isFalse(ReactDOM.findDOMNode(this.component).classList.contains('open'));
+        TestUtils.Simulate.click(component.refs['toggle-label']);
+        expect(ReactDOM.findDOMNode(component).classList.contains('open')).toBe(false);
       });
       it('closing', () => {
-        this.component = TestUtils.renderIntoDocument(<FieldsToggle model={this.model}
+        component = TestUtils.renderIntoDocument(<FieldsToggle model={model}
                                                                     name="Some Field"
                                                                     initialOpenState={false}
                                                                     attrsToStore={'analysisType'}
-                                                                    getFn={this.getFn}
-                                                                    updateFn={this.updateFn} />);
+                                                                    getFn={getFn}
+                                                                    updateFn={updateFn} />);
 
-        TestUtils.Simulate.click(this.component.refs['toggle-label']);
-        assert.isTrue(ReactDOM.findDOMNode(this.component).classList.contains('open'));
+        TestUtils.Simulate.click(component.refs['toggle-label']);
+        expect(ReactDOM.findDOMNode(component).classList.contains('open')).toBe(true);
       });
     });
   });
@@ -114,36 +101,36 @@ describe('components/common/fields_toggle', () => {
     describe('storing attributes from the model', () => {
 
       it('properly stores a single attribute', () => {
-        this.component = TestUtils.renderIntoDocument(<FieldsToggle model={this.model}
+        component = TestUtils.renderIntoDocument(<FieldsToggle model={model}
                                                                     name="Some Field"
                                                                     initialOpenState={true}
                                                                     attrsToStore={'some_attr'}
                                                                     getFn={() => {
                                                                       return 'something to be stored';
                                                                     }}
-                                                                    updateFn={this.updateFn} />);
-        this.component.toggle();
-        assert.deepEqual(this.component._storedAttrs, {
+                                                                    updateFn={updateFn} />);
+        component.toggle();
+        expect(component._storedAttrs).toEqual({
           some_attr: 'something to be stored'
         });
       });
 
       it('properly stores multiple attributes', () => {
-        this.model.analysis_type = 'something to be stored';
-        this.model.event_collection = 'some kind of collection';
+        model.analysis_type = 'something to be stored';
+        model.event_collection = 'some kind of collection';
 
-        this.component = TestUtils.renderIntoDocument(<FieldsToggle model={this.model}
+        component = TestUtils.renderIntoDocument(<FieldsToggle model={model}
                                                                     name="Some Field"
                                                                     initialOpenState={true}
                                                                     attrsToStore={['analysis_type', 'event_collection']}
                                                                     getFn={function(attrName){
-                                                                      return this.model[attrName];
+                                                                      return model[attrName];
                                                                     }}
-                                                                    updateFn={this.updateFn} />);
+                                                                    updateFn={updateFn} />);
 
-        this.component.toggle();
+        component.toggle();
 
-        assert.deepEqual(this.component._storedAttrs, {
+        expect(component._storedAttrs).toEqual({
           analysis_type: 'something to be stored',
           event_collection: 'some kind of collection'
         });
@@ -154,39 +141,40 @@ describe('components/common/fields_toggle', () => {
     describe('restoring attributes to the model', () => {
 
       it('properly restores a single attribute', () => {
-        this.component = TestUtils.renderIntoDocument(<FieldsToggle model={this.model}
+        component = TestUtils.renderIntoDocument(<FieldsToggle model={model}
                                                                     name="Some Field"
                                                                     initialOpenState={false}
                                                                     attrsToStore={'analysis_type'}
-                                                                    updateFn={this.updateFn}
+                                                                    updateFn={updateFn}
                                                                     getFn={() => {
-                                                                      return this.model.analysis_type;
+                                                                      return model.analysis_type;
                                                                     }}
-                                                                    updateFn={this.updateFn} />);
+                                                                    updateFn={updateFn} />);
 
-        this.component._storedAttrs.analysis_type = 'something to be stored';
-        this.component.toggle();
-        assert.deepPropertyVal(this.updateFn.getCall(0).args[0], 'analysis_type', 'something to be stored');
+        component._storedAttrs.analysis_type = 'something to be stored';
+        component.toggle();
+        expect(updateFn.mock.calls[0][0]).toEqual({
+          'analysis_type': 'something to be stored'
+        });
       });
 
       it('properly restores multiple attributes', () => {
-        this.component = TestUtils.renderIntoDocument(<FieldsToggle model={this.model}
+        component = TestUtils.renderIntoDocument(<FieldsToggle model={model}
                                                                     name="Some Field"
                                                                     initialOpenState={false}
                                                                     attrsToStore={['analysis_type', 'event_collection']}
-                                                                    updateFn={this.updateFn}
+                                                                    updateFn={updateFn}
                                                                     getFn={function(name){
-                                                                      return this.model[name];
+                                                                      return model[name];
                                                                     }}
-                                                                    updateFn={this.updateFn} />);
+                                                                    updateFn={updateFn} />);
 
-        this.component._storedAttrs = {
+        component._storedAttrs = {
           analysis_type: 'something to be stored',
           event_collection: 'some kind of collection'
         };
-        this.component.toggle();
-        assert.deepPropertyVal(this.updateFn.getCall(0).args[0], 'analysis_type', 'something to be stored');
-        assert.deepPropertyVal(this.updateFn.getCall(0).args[0], 'event_collection', 'some kind of collection');
+        component.toggle();
+        expect(updateFn.mock.calls[0][0]).toEqual(component._storedAttrs);
       });
 
     });
@@ -194,36 +182,38 @@ describe('components/common/fields_toggle', () => {
     describe('resetting attributes from the model', () => {
 
       it('properly resets a single attribute', () => {
-        this.component = TestUtils.renderIntoDocument(<FieldsToggle model={this.model}
+        component = TestUtils.renderIntoDocument(<FieldsToggle model={model}
                                                                     name="Some Field"
                                                                     initialOpenState={true}
                                                                     attrsToStore={'analysis_type'}
                                                                     resetValues={{analysis_type: 'a reset value'}}
                                                                     getFn={function(name){
-                                                                      return this.model.analysis_type;
+                                                                      return model.analysis_type;
                                                                     }}
-                                                                    updateFn={this.updateFn} />);
-        this.component.toggle();
-        assert.deepPropertyVal(this.updateFn.getCall(0).args[0], 'analysis_type', 'a reset value');
+                                                                    updateFn={updateFn} />);
+        component.toggle();
+        expect(updateFn.mock.calls[0][0]).toEqual({
+          'analysis_type': 'a reset value'
+        });
       });
 
       it('properly resets a multiple attributes', () => {
-        this.component = TestUtils.renderIntoDocument(<FieldsToggle model={this.model}
+        const resetValues = {
+          analysis_type: 'analysis_type reset value',
+          event_collection: 'event_collection reset value'
+        };
+        component = TestUtils.renderIntoDocument(<FieldsToggle model={model}
                                                                     name="Some Field"
                                                                     initialOpenState={true}
                                                                     attrsToStore={['analysis_type', 'event_collection']}
-                                                                    resetValues={{
-                                                                      analysis_type: 'analysis_type reset value',
-                                                                      event_collection: 'event_collection reset value'
-                                                                    }}
+                                                                    resetValues={resetValues}
                                                                     getFn={function(name) {
-                                                                      return this.model[name];
+                                                                      return model[name];
                                                                     }}
-                                                                    updateFn={this.updateFn} />);
+                                                                    updateFn={updateFn} />);
 
-        this.component.toggle();
-        assert.deepPropertyVal(this.updateFn.getCall(0).args[0], 'analysis_type', 'analysis_type reset value');
-        assert.deepPropertyVal(this.updateFn.getCall(0).args[0], 'event_collection', 'event_collection reset value');
+        component.toggle();
+        expect(updateFn.mock.calls[0][0]).toEqual(resetValues);
       });
 
     });
