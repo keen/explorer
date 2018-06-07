de78b87453ccd6ba41897fba30d19ec78cf95dc7
Adam Kasprowicz <adam.kasprowicz@keen.io>
2018-06-07 15:16:37 +0200

    fix tests for explorer visualization chart

--- test/unit/components/explorer/visualization/chart_spec.js
+++ test/unit/components/explorer/visualization/chart_spec.js
@@ -1,58 +1,59 @@
-
-var _ from 'lodash');
-let sinon from 'sinon/pkg/sinon.js');
-var Chart from '../../../../../lib/js/app/components/explorer/visualization/chart.js');
+import _ from 'lodash';
+import Chart from '../../../../../lib/js/app/components/explorer/visualization/chart.js';
 import React from 'react';
-var TestUtils from 'react-addons-test-utils');
-var ExplorerUtils from '../../../../../lib/js/app/utils/ExplorerUtils');
-var FormatUtils from '../../../../../lib/js/app/utils/FormatUtils');
-var TestHelpers from '../../../../support/TestHelpers');
+import TestUtils from 'react-addons-test-utils';
+import ExplorerUtils from '../../../../../lib/js/app/utils/ExplorerUtils';
+import FormatUtils from '../../../../../lib/js/app/utils/FormatUtils';
+import TestHelpers from '../../../../support/TestHelpers';
 
 describe('components/explorer/visualization/chart', () => {
+  let model;
+  let dataviz;
+  let component;
 
   beforeEach(() => {
-    this.model = TestHelpers.createExplorerModel();
-    this.dataviz = TestHelpers.createDataviz();
-    this.component = TestUtils.renderIntoDocument(<Chart model={this.model} dataviz={this.dataviz} />);
+    model = TestHelpers.createExplorerModel();
+    dataviz = TestHelpers.createDataviz();
+    component = TestUtils.renderIntoDocument(<Chart model={model} dataviz={dataviz} />);
   });
 
   describe('setup', () => {
     it('is of the right type', () => {
-      assert.isTrue(TestUtils.isCompositeComponentWithType(this.component, Chart));
+      expect(TestUtils.isCompositeComponentWithType(component, Chart)).toBe(true);
     });
     it('has a get started message when there is no query yet', () => {
       var message = "Let's go exploring!";
-      assert.equal(this.component.refs.notice.textContent, message);
+      expect(component.refs.notice.textContent).toEqual(message);
     });
     it('shows the correct message about email extractions ', () => {
-      this.model.query.analysis_type = 'extraction';
-      this.model.query.email = 'someone@keen.io';
-      this.model.response = { result: 10, success: true };
-      this.component = TestUtils.renderIntoDocument(<Chart model={this.model} dataviz={this.dataviz} />);
+      model.query.analysis_type = 'extraction';
+      model.query.email = 'someone@keen.io';
+      model.response = { result: 10, success: true };
+      component = TestUtils.renderIntoDocument(<Chart model={model} dataviz={dataviz} />);
       var message = "Email extractions don't have visualizations.";
-      assert.equal(this.component.refs.notice.textContent, message);
+      expect(component.refs.notice.textContent).toEqual(message);
     });
   });
 
   describe('JSON viz', () => {
     it('should only show the query result', () => {
-      this.model.response = {
+      model.response = {
         query: { do_not: 'show_me' },
         result: 100
       };
-      this.model.metadata.visualization.chart_type = 'json';
-      this.component = TestUtils.renderIntoDocument(<Chart model={this.model} dataviz={this.dataviz} />);
-      assert.strictEqual(this.component.refs.jsonViz.textContent, FormatUtils.prettyPrintJSON({ result: 100 }));
+      model.metadata.visualization.chart_type = 'json';
+      component = TestUtils.renderIntoDocument(<Chart model={model} dataviz={dataviz} />);
+      expect(component.refs.jsonViz.textContent).toEqual(FormatUtils.prettyPrintJSON({ result: 100 }));
     });
     it('should also show the actors property if present in the response', () => {
-      this.model.response = {
+      model.response = {
         query: { do_not: 'show_me' },
         result: 100,
         actors: ['a', 'b', 'c']
       };
-      this.model.metadata.visualization.chart_type = 'json';
-      this.component = TestUtils.renderIntoDocument(<Chart model={this.model} dataviz={this.dataviz} />);
-      assert.strictEqual(this.component.refs.jsonViz.textContent, FormatUtils.prettyPrintJSON({ result: 100, actors: ['a', 'b', 'c'] }));
+      model.metadata.visualization.chart_type = 'json';
+      component = TestUtils.renderIntoDocument(<Chart model={model} dataviz={dataviz} />);
+      expect(component.refs.jsonViz.textContent).toEqual(FormatUtils.prettyPrintJSON({ result: 100, actors: ['a', 'b', 'c'] }));
     });
   });
 });
