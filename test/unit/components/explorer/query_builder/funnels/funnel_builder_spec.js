d8b17d562bf6da4d6483432dd762d4cda0cf0ca0
Adam Kasprowicz <adam.kasprowicz@keen.io>
2018-06-07 14:53:55 +0200

    fix test funnel builder

--- test/unit/components/explorer/query_builder/funnels/funnel_builder_spec.js
+++ test/unit/components/explorer/query_builder/funnels/funnel_builder_spec.js
@@ -1,70 +1,72 @@
-var _ from 'lodash');
 
+import ExplorerActions from '../../../../../../lib/js/app/actions/ExplorerActions.js';
+import FunnelStep from '../../../../../../lib/js/app/components/explorer/query_builder/funnels/funnel_step.js';
+import FunnelBuilder from '../../../../../../lib/js/app/components/explorer/query_builder/funnels/funnel_builder.js';
+
+import _ from 'lodash';
 import React from 'react';
-var ReactDOM from 'react-dom');
-var TestUtils from 'react-addons-test-utils');
-let sinon from 'sinon/pkg/sinon.js');
-var $R from 'rquery')(_, React, ReactDOM, TestUtils);
-var TestHelpers from '../../../../../support/TestHelpers');
-var ExplorerActions from '../../../../../../lib/js/app/actions/ExplorerActions.js');
-var FunnelStep from '../../../../../../lib/js/app/components/explorer/query_builder/funnels/funnel_step.js');
-var FunnelBuilder from '../../../../../../lib/js/app/components/explorer/query_builder/funnels/funnel_builder.js');
+import ReactDOM from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
+import TestHelpers from '../../../../../../test/support/TestHelpers';
+import rquery from 'rquery';
 
-function getProps(props) {
-  var props = props || {};
-  var defaults = {
+const $R = rquery(_, React, ReactDOM, TestUtils);
+
+function getProps(props = {}) {
+  const defaults = {
     modelId: 'abc123',
     steps: [],
-    onBrowseEvents: sinon.stub(),
+    onBrowseEvents: jest.fn(),
     eventCollections: [],
     stepNotices: [],
-    getEventPropertyNames: sinon.stub().returns([]),
-    getPropertyType: sinon.stub().returns('String')
+    getEventPropertyNames: jest.fn().mockReturnValue([]),
+    getPropertyType: jest.fn().mockReturnValue('String')
   };
   return _.assign({}, defaults, props);
 }
 
 describe('components/explorer/query_builder/funnels/funnel_builder', () => {
+  let component;
 
   it('should render as many FunnelStep child components as there are steps', () => {
-    this.component = TestHelpers.renderComponent(FunnelBuilder, getProps({
+    component = TestHelpers.renderComponent(FunnelBuilder, getProps({
       steps: [
         TestHelpers.createStep(),
         TestHelpers.createStep(),
         TestHelpers.createStep()
       ]
     }));
-    assert.lengthOf(TestUtils.scryRenderedComponentsWithType(this.component, FunnelStep), 3);
+    expect(TestUtils.scryRenderedComponentsWithType(component, FunnelStep)).toHaveLength(3);
   });
 
   it('should call ExplorerActions.addStep when the add step link is clicked', () => {
-    this.component = TestHelpers.renderComponent(FunnelBuilder, getProps({
+    component = TestHelpers.renderComponent(FunnelBuilder, getProps({
       steps: [ TestHelpers.createStep() ]
     }));
-    var stub = sinon.stub(ExplorerActions, 'addStep');
-    TestUtils.Simulate.click($R(this.component).find('.add-step').components[0]);
-    assert.isTrue(stub.calledOnce);
-    ExplorerActions.addStep.restore();
+    const stub = jest.spyOn(ExplorerActions, 'addStep').mockImplementation(()=>{});
+    TestUtils.Simulate.click($R(component).find('.add-step').components[0]);
+    expect(stub).toHaveBeenCalled();
+    stub.mockRestore();
   });
 
   it('should toggle a step active if it is inactive and the header is clicked', () => {
-    this.component = TestHelpers.renderComponent(FunnelBuilder, getProps({
+    component = TestHelpers.renderComponent(FunnelBuilder, getProps({
       steps: [ TestHelpers.createStep() ]
     }));
-    var stub = sinon.stub(ExplorerActions, 'setStepActive');
-    TestUtils.Simulate.click($R(this.component).find('li .step-header').components[0]);
-    assert.isTrue(stub.calledOnce);
-    ExplorerActions.setStepActive.restore();
+    const stub = jest.spyOn(ExplorerActions, 'setStepActive').mockImplementation(()=>{});
+    TestUtils.Simulate.click($R(component).find('li .step-header').components[0]);
+    expect(stub).toHaveBeenCalled();
+    stub.mockRestore();
   });
 
   it('should toggle a step inactive if it is active and the header is clicked', () => {
-    this.component = TestHelpers.renderComponent(FunnelBuilder, getProps({
+    component = TestHelpers.renderComponent(FunnelBuilder, getProps({
       steps: [ _.assign(TestHelpers.createStep(), { active: true }) ]
     }));
-    var stub = sinon.stub(ExplorerActions, 'updateStep');
-    TestUtils.Simulate.click($R(this.component).find('li .step-header').components[0]);
-    assert.isTrue(stub.calledWith('abc123', 0, { active: false }));
-    ExplorerActions.updateStep.restore();
+    const stub = jest.spyOn(ExplorerActions, 'updateStep').mockImplementation(()=>{});
+    TestUtils.Simulate.click($R(component).find('li .step-header').components[0]);
+    expect(stub).toHaveBeenCalledWith('abc123', 0, { active: false });
+    stub.mockRestore();
   });
 
 });
