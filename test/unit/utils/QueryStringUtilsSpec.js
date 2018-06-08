4365fde2c6cde8b741064d1f125bcf5877ea5bab
Adam Kasprowicz <adam.kasprowicz@keen.io>
2018-06-08 13:56:27 +0200

    fix tests for query string util

--- test/unit/utils/QueryStringUtilsSpec.js
+++ test/unit/utils/QueryStringUtilsSpec.js
@@ -1,15 +1,13 @@
-
-let sinon from 'sinon/pkg/sinon.js');
-var QueryStringUtils from '../../../lib/js/app/utils/QueryStringUtils');
+import QueryStringUtils from '../../../lib/js/app/utils/QueryStringUtils';
 
 describe('utils/QueryStringUtils', () => {
-  
+
   describe('getQueryAttributes', () => {
-    
+
     it('should properly parse funnel step filters', () => {
-      sinon.stub(QueryStringUtils, 'getSearchString').returns('query[steps][0][filters][0][property_value][0]=testing');
-      assert.sameMembers(QueryStringUtils.getQueryAttributes().query.steps[0].filters[0].property_value, ['testing']);
-      QueryStringUtils.getSearchString.restore();
+      const spy = jest.spyOn(QueryStringUtils, 'getSearchString').mockReturnValue('query[steps][0][filters][0][property_value][0]=testing');
+      expect(QueryStringUtils.getQueryAttributes().query.steps[0].filters[0].property_value).toEqual(['testing']);
+      spy.mockRestore();
     });
 
   });
