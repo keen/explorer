84aef41fb89e1197d2a86f72d5eb5a36652aab28
Adam Kasprowicz <adam.kasprowicz@keen.io>
2018-06-08 14:07:29 +0200

    fix tests for timeframe utils

--- test/unit/utils/TimeframeUtilsSpec.js
+++ test/unit/utils/TimeframeUtilsSpec.js
@@ -1,79 +1,75 @@
-
-var expect from 'chai').expect;
-var moment from 'moment');
-let sinon from 'sinon/pkg/sinon.js');
-var TimeframeUtils from '../../../lib/js/app/utils/TimeframeUtils');
+import moment from 'moment';
+import TimeframeUtils from '../../../lib/js/app/utils/TimeframeUtils';
 
 describe('utils/TimeframeUtils', () => {
 
   describe('getTimeframe', () => {
     it('should call the right timeframe builder for absolute timeframes', () => {
-      var time = {
+      const time = {
         start: new Date(moment().subtract(1, 'days').startOf('day').format()),
         end: new Date(moment().startOf('day').format())
       };
-      var stub = sinon.stub(TimeframeUtils.timeframeBuilders, 'absolute');
+      const stub = jest.spyOn(TimeframeUtils.timeframeBuilders, 'absolute');
 
       TimeframeUtils.getTimeframe(time, 'US/Hawaii');
-      assert.isTrue(stub.calledOnce);
-      TimeframeUtils.timeframeBuilders.absolute.restore();
+      expect(stub).toHaveBeenCalledTimes(1);
+      stub.mockRestore();
     });
     it('should call the right timeframe builder for relative timeframes', () => {
-      var time = {
+      const time = {
         relativity: 'this',
         amount: '1',
         sub_timeframe: 'days'
       };
-      var stub = sinon.stub(TimeframeUtils.timeframeBuilders, 'relative');
+      const stub = jest.spyOn(TimeframeUtils.timeframeBuilders, 'relative');
 
       TimeframeUtils.getTimeframe(time);
-      assert.isTrue(stub.calledOnce);
-      TimeframeUtils.timeframeBuilders.relative.restore();
+      expect(stub).toHaveBeenCalledTimes(1);
+      stub.mockRestore();
     });
   });
 
   describe('timeframeBuilders', () => {
     describe('absolute', () => {
       it('should properly build a timeframe object', () => {
-        var time = {
+        const time = {
           start: new Date(moment().subtract(1, 'days').startOf('day').format()),
           end: new Date(moment().startOf('day').format())
         };
-        var timeframe = TimeframeUtils.getTimeframe(time, 'US/Hawaii');
+        const timeframe = TimeframeUtils.getTimeframe(time, 'US/Hawaii');
 
-        var expectedFormat = 'YYYY-MM-DDTHH:mm:ss.SSS';
-        var expectedStart = moment(new Date(time.start)).format(expectedFormat);
-        var expectedEnd = moment(new Date(time.end)).format(expectedFormat);
+        const expectedFormat = 'YYYY-MM-DDTHH:mm:ss.SSS';
+        const expectedStart = moment(new Date(time.start)).format(expectedFormat);
+        const expectedEnd = moment(new Date(time.end)).format(expectedFormat);
 
-        assert.deepEqual(timeframe, {
+        expect(timeframe).toEqual({
           start: expectedStart,
           end: expectedEnd
         });
       });
     });
     describe('relative', () => {
-      var time = {
+      const time = {
         relativity: 'this',
         amount: '1',
         sub_timeframe: 'days'
       };
 
-      var timeframe = TimeframeUtils.getTimeframe(time);
-      assert.deepEqual(timeframe, 'this_1_days');
+      const timeframe = TimeframeUtils.getTimeframe(time);
+      expect(timeframe).toEqual('this_1_days');
     });
   });
 
   describe('unpackTimeframeParam', () => {
     it('properly unpacks an absolute timeframe', () => {
-      var timeframe = {
+      const timeframe = {
         start: moment(new Date("June 7, 2015 1:00 PM")).format('YYYY-MM-DDTHH:mm:ss.SSS')+"-10:00",
         end: moment(new Date("June 8, 2015 2:00 PM")).format('YYYY-MM-DDTHH:mm:ss.SSS')+"-10:00"
       };
 
       console.log(timeframe.start);
 
-      assert.deepEqual(
-        TimeframeUtils.unpackTimeframeParam(timeframe, 'US/Hawaii'),
+      expect(TimeframeUtils.unpackTimeframeParam(timeframe, 'US/Hawaii')).toEqual(
         {
           timezone: 'US/Hawaii',
           time: {
@@ -84,9 +80,9 @@ describe('utils/TimeframeUtils', () => {
       );
     });
     it('properly unpacks a relative timeframe', () => {
-      var unpacked = TimeframeUtils.unpackTimeframeParam('this_8_days', 'Europe/London');
+      const unpacked = TimeframeUtils.unpackTimeframeParam('this_8_days', 'Europe/London');
 
-      assert.deepEqual(unpacked, {
+      expect(unpacked).toEqual({
         time: {
           relativity: 'this',
           amount: '8',
@@ -99,62 +95,62 @@ describe('utils/TimeframeUtils', () => {
 
   describe('timeframeType', () => {
     it('recognizes relative timeframes', () => {
-      var relative_timeframe = {
+      const relative_timeframe = {
         relativity: 'this',
         amount: '8',
         sub_timeframe: 'days'
       }
 
-      assert.equal(TimeframeUtils.timeframeType(relative_timeframe), 'relative');
+      expect(TimeframeUtils.timeframeType(relative_timeframe)).toEqual('relative');
     });
 
     it('recognizes absolute timeframes', () => {
-      var absolute_timeframe = {
+      const absolute_timeframe = {
         start: moment().subtract(2, 'days').format(),
         end: moment().subtract(1, 'days').format()
       }
 
-      assert.equal(TimeframeUtils.timeframeType(absolute_timeframe), 'absolute');
+      expect(TimeframeUtils.timeframeType(absolute_timeframe)).toEqual('absolute');
     });
 
     describe('invalid timeframes', () => {
       it('throws error for invalid time object', () => {
-        expect(TimeframeUtils.timeframeType.bind(window, {bee: 'boop'})).to.throw("Invalid timeframe type: invalid time value.");
+        expect(TimeframeUtils.timeframeType.bind(window, {bee: 'boop'})).toThrow("Invalid timeframe type: invalid time value.");
       });
 
       it('throws errors for non-object time objects', () => {
-        expect(TimeframeUtils.timeframeType.bind(window, 'this_3_days')).to.throw("Invalid timeframe type: not a plain object.");
+        expect(TimeframeUtils.timeframeType.bind(window, 'this_3_days')).toThrow("Invalid timeframe type: not a plain object.");
       })
     });
   });
 
   describe('getTimeParameters', () => {
     it('returns whatever getTimeframe responds with', () => {
-      var expectedResult = {cool: 'dude'};
+      const expectedResult = {cool: 'dude'};
 
-      var stub = sinon.stub(TimeframeUtils, 'getTimeframe').returns(expectedResult);
-      sinon.stub(TimeframeUtils, 'timeframeType').returns('absolute');
+      const stubGetTimeframe = jest.spyOn(TimeframeUtils, 'getTimeframe').mockReturnValue(expectedResult);
+      const stubTimeframeType = jest.spyOn(TimeframeUtils, 'timeframeType').mockReturnValue('absolute');
 
-      assert.deepPropertyVal(TimeframeUtils.getTimeParameters({}, 'US/Hawaii'), 'timeframe', expectedResult);
+      expect(TimeframeUtils.getTimeParameters({}, 'US/Hawaii').timeframe).toEqual(expectedResult);
 
-      TimeframeUtils.getTimeframe.restore();
-      TimeframeUtils.timeframeType.restore();
+      stubGetTimeframe.mockRestore();
+      stubTimeframeType.mockRestore();
     });
 
     it('returns the same timezone if the timeframeType is relative', () => {
-      var stub = sinon.stub(TimeframeUtils, 'timeframeType').returns('relative');
+      const stub = jest.spyOn(TimeframeUtils, 'timeframeType').mockReturnValue('relative');
 
-      assert.deepPropertyVal(TimeframeUtils.getTimeParameters({
+      expect(TimeframeUtils.getTimeParameters({
         relativity: 'this',
         amount: 1,
         sub_timeframe: 'days'
-      }, 'US/Hawaii'), 'timezone', 'US/Hawaii');
+      }, 'US/Hawaii').timezone).toEqual('US/Hawaii');
 
-      TimeframeUtils.timeframeType.restore();
+      stub.mockRestore();
     });
 
     it('returns null timeframe with default timezone if passed undefined values', () => {
-      assert.deepEqual(TimeframeUtils.getTimeParameters(), {
+      expect(TimeframeUtils.getTimeParameters()).toEqual({
         timeframe: null,
         timezone: 'UTC'
       });
