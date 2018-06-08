7de85ce28c6141a62eac168615476beb8a86860f
Adam Kasprowicz <adam.kasprowicz@keen.io>
2018-06-08 14:10:50 +0200

    fix tests for step validations

--- test/unit/validations/StepValidationsSpec.js
+++ test/unit/validations/StepValidationsSpec.js
@@ -1,90 +1,87 @@
-
-var _ from 'lodash');
-let sinon from 'sinon/pkg/sinon.js');
-var moment from 'moment');
-var TestHelpers from '../../support/TestHelpers');
-var StepValidations from '../../../lib/js/app/validations/StepValidations');
+import _ from 'lodash';
+import TestHelpers from '../../support/TestHelpers';
+import StepValidations from '../../../lib/js/app/validations/StepValidations';
 
 describe('validations/StepValidations', () => {
 
   describe('optional', () => {
     it('has an error message', () => {
-      var errorMessage = StepValidations.optional.msg;
-      assert.equal(errorMessage, 'You must select whether this step is optional.');
+      const errorMessage = StepValidations.optional.msg;
+      expect(errorMessage).toEqual('You must select whether this step is optional.');
     });
 
     it('returns true when the value is true', () => {
-      assert.isTrue(StepValidations.optional.validate({ optional: true }));
+      expect(StepValidations.optional.validate({ optional: true })).toBe(true);
     });
 
     it('returns true when the value is false', () => {
-      assert.isTrue(StepValidations.optional.validate({ optional: false }));
+      expect(StepValidations.optional.validate({ optional: false })).toBe(true);
     });
 
     it('returns false when the value is null', () => {
-      assert.isFalse(StepValidations.optional.validate({ optional: null }));
+      expect(StepValidations.optional.validate({ optional: null })).toBe(false);
     });
 
     it('returns false when the value is undefined', () => {
-      assert.isFalse(StepValidations.optional.validate({ optional: undefined }));
+      expect(StepValidations.optional.validate({ optional: undefined })).toBe(false);
     });
 
     it('returns false when the value is not a boolean', () => {
-      assert.isFalse(StepValidations.optional.validate({ optional: 'nope' }));
+      expect(StepValidations.optional.validate({ optional: 'nope' })).toBe(false);
     });
   });
 
   describe('inverted', () => {
     it('has an error message', () => {
-      var errorMessage = StepValidations.inverted.msg;
-      assert.equal(errorMessage, 'You must select whether this step is inverted.');
+      const errorMessage = StepValidations.inverted.msg;
+      expect(errorMessage).toEqual('You must select whether this step is inverted.');
     });
 
     it('returns true when the value is true', () => {
-      assert.isTrue(StepValidations.inverted.validate({ inverted: true }));
+      expect(StepValidations.inverted.validate({ inverted: true })).toBe(true);
     });
 
     it('returns true when the value is false', () => {
-      assert.isTrue(StepValidations.inverted.validate({ inverted: false }));
+      expect(StepValidations.inverted.validate({ inverted: false })).toBe(true);
     });
 
     it('returns false when the value is null', () => {
-      assert.isFalse(StepValidations.inverted.validate({ inverted: null }));
+      expect(StepValidations.inverted.validate({ inverted: null })).toBe(false);
     });
 
     it('returns false when the value is undefined', () => {
-      assert.isFalse(StepValidations.inverted.validate({ inverted: undefined }));
+      expect(StepValidations.inverted.validate({ inverted: undefined })).toBe(false);
     });
 
     it('returns false when the value is not a boolean', () => {
-      assert.isFalse(StepValidations.inverted.validate({ inverted: 'nope' }));
+      expect(StepValidations.inverted.validate({ inverted: 'nope' })).toBe(false);
     });
   });
 
   describe('with_actors', () => {
     it('has an error message', () => {
-      var errorMessage = StepValidations.with_actors.msg;
-      assert.equal(errorMessage, '"with_actors" must be set to either true or false');
+      const errorMessage = StepValidations.with_actors.msg;
+      expect(errorMessage).toEqual('"with_actors" must be set to either true or false');
     });
 
     it('returns true when the value is true', () => {
-      assert.isTrue(StepValidations.with_actors.validate({ with_actors: true }));
+      expect(StepValidations.with_actors.validate({ with_actors: true })).toBe(true);
     });
 
     it('returns true when the value is false', () => {
-      assert.isTrue(StepValidations.with_actors.validate({ with_actors: false }));
+      expect(StepValidations.with_actors.validate({ with_actors: false })).toBe(true);
     });
 
     it('returns false when the value is null', () => {
-      assert.isFalse(StepValidations.with_actors.validate({ with_actors: null }));
+      expect(StepValidations.with_actors.validate({ with_actors: null })).toBe(false);
     });
 
     it('returns false when the value is undefined', () => {
-      assert.isFalse(StepValidations.with_actors.validate({ with_actors: undefined }));
+      expect(StepValidations.with_actors.validate({ with_actors: undefined })).toBe(false);
     });
 
     it('returns false when the value is not a boolean', () => {
-      assert.isFalse(StepValidations.with_actors.validate({ with_actors: 'nope' }));
+      expect(StepValidations.with_actors.validate({ with_actors: 'nope' })).toBe(false);
     });
   });
 
