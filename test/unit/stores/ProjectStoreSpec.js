7ccd5ae469035e622cf8f7630a46ad3ee0a6d1fd
Adam Kasprowicz <adam.kasprowicz@keen.io>
2018-06-08 12:59:05 +0200

    fix tests project store

--- test/unit/stores/ProjectStoreSpec.js
+++ test/unit/stores/ProjectStoreSpec.js
@@ -1,12 +1,10 @@
-
-var _ from 'lodash');
-let sinon from 'sinon/pkg/sinon.js');
-var moment from 'moment');
-var TestHelpers from '../../support/TestHelpers');
-var ProjectUtils from '../../../lib/js/app/utils/ProjectUtils');
-var FormatUtils from '../../../lib/js/app/utils/FormatUtils');
-var ProjectStore from '../../../lib/js/app/stores/ProjectStore');
-var ProjectActions from '../../../lib/js/app/actions/ProjectActions');
+import _ from 'lodash';
+import moment from 'moment';
+import TestHelpers from '../../support/TestHelpers';
+import ProjectUtils from '../../../lib/js/app/utils/ProjectUtils';
+import FormatUtils from '../../../lib/js/app/utils/FormatUtils';
+import ProjectStore from '../../../lib/js/app/stores/ProjectStore';
+import ProjectActions from '../../../lib/js/app/actions/ProjectActions';
 
 describe('stores/ProjectStore', () => {
 
@@ -17,9 +15,8 @@ describe('stores/ProjectStore', () => {
   describe('_create', () => {
     it('should create a new project with the default attributes', () => {
       ProjectActions.create();
-      var keys = Object.keys(ProjectStore.getAll());
-      assert.deepEqual(
-        _.omit(ProjectStore.getAll()[keys[0]], 'id'),
+      const keys = Object.keys(ProjectStore.getAll());
+      expect(_.omit(ProjectStore.getAll()[keys[0]], 'id')).toEqual(
         {
           loading: true,
           eventCollections: [],
@@ -35,9 +32,8 @@ describe('stores/ProjectStore', () => {
           name: 'clicks'
         }
       });
-      var keys = Object.keys(ProjectStore.getAll());
-      assert.deepEqual(
-        _.omit(ProjectStore.getAll()[keys[0]], 'id'),
+      const keys = Object.keys(ProjectStore.getAll());
+      expect(_.omit(ProjectStore.getAll()[keys[0]], 'id')).toEqual(
         {
           loading: false,
           eventCollections: [],
@@ -53,15 +49,14 @@ describe('stores/ProjectStore', () => {
   describe('_update', () => {
     it('should update the expected project with the provided attributes', () => {
       ProjectActions.create();
-      var keys = Object.keys(ProjectStore.getAll());
+      const keys = Object.keys(ProjectStore.getAll());
       ProjectActions.update(keys[0], {
         loading: false,
         schema: {
           name: 'clicks'
         }
       });
-      assert.deepEqual(
-        _.omit(ProjectStore.getAll()[keys[0]], 'id'),
+      expect(_.omit(ProjectStore.getAll()[keys[0]], 'id')).toEqual(
         {
           loading: false,
           eventCollections: [],
@@ -77,8 +72,8 @@ describe('stores/ProjectStore', () => {
   describe('getProject', () => {
     it('should return the first project', () => {
       ProjectActions.create();
-      var keys = Object.keys(ProjectStore.getAll());
-      assert.deepEqual(ProjectStore.getProject(), ProjectStore.getAll()[keys[0]]);
+      const keys = Object.keys(ProjectStore.getAll());
+      expect(ProjectStore.getProject()).toEqual(ProjectStore.getAll()[keys[0]]);
     });
   });
 
