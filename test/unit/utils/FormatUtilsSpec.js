7f2763db89684762efeabd2cd21f3eaf186a0826
Adam Kasprowicz <adam.kasprowicz@keen.io>
2018-06-08 13:47:31 +0200

    fix test for format utils

--- test/unit/utils/FormatUtilsSpec.js
+++ test/unit/utils/FormatUtilsSpec.js
@@ -1,187 +1,184 @@
-
-var expect from 'chai').expect;
-var _ from 'lodash');
-let sinon from 'sinon/pkg/sinon.js');
-var moment from 'moment');
-var TestHelpers from '../../support/TestHelpers');
-var FormatUtils from '../../../lib/js/app/utils/FormatUtils');
+import _ from 'lodash';
+import moment from 'moment';
+import TestHelpers from '../../support/TestHelpers';
+import FormatUtils from '../../../lib/js/app/utils/FormatUtils';
 
 describe('utils/FormatUtils', () => {
   describe('toTitleCase', () => {
     it('transforms text to titleCase', () => {
-      assert.equal(FormatUtils.toTitleCase('keenIO'), 'Keenio');
+      expect(FormatUtils.toTitleCase('keenIO')).toEqual('Keenio');
     });
   });
 
   describe('singularize', () => {
     it('transforms text to singular', () => {
-      assert.equal(FormatUtils.singularize('analytics'), 'analytic');
+      expect(FormatUtils.singularize('analytics')).toEqual('analytic');
     });
   });
 
   describe('prettyPrintJSON', () => {
     it('stringifies the json', () => {
-      var json = 'json';
-      var stringifySpy = sinon.spy(JSON, 'stringify');
+      const json = 'json';
+      const stringifySpy = jest.spyOn(JSON, 'stringify');
       FormatUtils.prettyPrintJSON(json);
-      assert.isTrue(stringifySpy.calledWith(json, undefined, 2));
+      expect(stringifySpy).toHaveBeenCalledWith(json, undefined, 2);
     });
   });
 
   describe('sortItems', () => {
     it('sorts things', () => {
-      var sortedItems = FormatUtils.sortItems(['b', 'a']);
-      assert.deepEqual(sortedItems, ['a', 'b']);
+      const sortedItems = FormatUtils.sortItems(['b', 'a']);
+      expect(sortedItems).toEqual(['a', 'b']);
     });
     // x'd because phantomJs < 2.0.0 localeCompare fails; 2.0.0 is released but
     // is not yet listed on npm.
     xit('sorts unicode properly', () => {
-      var sortedItems = FormatUtils.sortItems(['a', 'n', 'b', 'z', 'ñ']);
-      assert.deepEqual(sortedItems, ['a', 'b', 'n', 'ñ', 'z']);
+      const sortedItems = FormatUtils.sortItems(['a', 'n', 'b', 'z', 'ñ']);
+      expect(sortedItems).toEqual(['a', 'b', 'n', 'ñ', 'z']);
     });
     it('ignores dash, underscore, case, and period', () => {
-      var sortedItems = FormatUtils.sortItems(['-ad', 'a_e', 'A.f', 'a-c', 'Ab']);
-      assert.deepEqual(sortedItems, ['Ab', 'a-c', '-ad', 'a_e', 'A.f']);
+      const sortedItems = FormatUtils.sortItems(['-ad', 'a_e', 'A.f', 'a-c', 'Ab']);
+      expect(sortedItems).toEqual(['Ab', 'a-c', '-ad', 'a_e', 'A.f']);
     });
   })
 
   describe('coercionTypeForPropertyType', () => {
     it("returns 'String' for string", () => {
-      var propertyType = 'string';
-      assert.equal(FormatUtils.coercionTypeForPropertyType(propertyType), 'String');
+      const propertyType = 'string';
+      expect(FormatUtils.coercionTypeForPropertyType(propertyType)).toEqual('String');
     });
     it("returns 'Number' for num", () => {
-      var propertyType = 'num';
-      assert.equal(FormatUtils.coercionTypeForPropertyType(propertyType), 'Number');
+      const propertyType = 'num';
+      expect(FormatUtils.coercionTypeForPropertyType(propertyType)).toEqual('Number');
     });
     it("returns 'Datetime' for datetime", () => {
-      var propertyType = 'datetime';
-      assert.equal(FormatUtils.coercionTypeForPropertyType(propertyType), 'Datetime');
+      const propertyType = 'datetime';
+      expect(FormatUtils.coercionTypeForPropertyType(propertyType)).toEqual('Datetime');
     });
     it("returns 'List' for list", () => {
-      var propertyType = 'list';
-      assert.equal(FormatUtils.coercionTypeForPropertyType(propertyType), 'List');
+      const propertyType = 'list';
+      expect(FormatUtils.coercionTypeForPropertyType(propertyType)).toEqual('List');
     });
     it("returns 'Null' for null", () => {
-      var propertyType = 'null';
-      assert.equal(FormatUtils.coercionTypeForPropertyType(propertyType), 'Null');
+      const propertyType = 'null';
+      expect(FormatUtils.coercionTypeForPropertyType(propertyType)).toEqual('Null');
     });
     it("returns 'Boolean' for boolean", () => {
-      var propertyType = 'bool';
-      assert.equal(FormatUtils.coercionTypeForPropertyType(propertyType), 'Boolean');
+      const propertyType = 'bool';
+      expect(FormatUtils.coercionTypeForPropertyType(propertyType)).toEqual('Boolean');
     });
   });
 
   describe('formatISOTimeNoTimezone', () => {
     it('formats as ISO time without a timezone', () => {
-      var time = moment('2014-08-20 15:44').format();
-      assert.match(FormatUtils.formatISOTimeNoTimezone(time), /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}/);
+      const time = moment('2014-08-20 15:44').format();
+      expect(FormatUtils.formatISOTimeNoTimezone(time).match(/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}/)).not.toBe(null);
     });
   });
 
   describe('booleanMap', () => {
     it('maps correctly for strings', () => {
-      assert.strictEqual(FormatUtils.booleanMap('true'), 'true');
-      assert.strictEqual(FormatUtils.booleanMap('false'), 'false');
+      expect(FormatUtils.booleanMap('true')).toEqual('true');
+      expect(FormatUtils.booleanMap('false')).toEqual('false');
     });
 
     it('maps correctly for null', () => {
-      assert.strictEqual(FormatUtils.booleanMap(null), '');
+      expect(FormatUtils.booleanMap(null)).toEqual('');
     });
 
     it('maps correctly for booleans', () => {
-      assert.strictEqual(FormatUtils.booleanMap(true), 'true');
-      assert.strictEqual(FormatUtils.booleanMap(false), 'false');
+      expect(FormatUtils.booleanMap(true)).toEqual('true');
+      expect(FormatUtils.booleanMap(false)).toEqual('false');
     });
 
     it('maps correctly for truthy values', () => {
-      assert.strictEqual(FormatUtils.booleanMap(1), 'true');
-      assert.strictEqual(FormatUtils.booleanMap('abc'), 'true');
+      expect(FormatUtils.booleanMap(1)).toEqual('true');
+      expect(FormatUtils.booleanMap('abc')).toEqual('true');
     });
   });
 
   describe('isValidQueryValue', () => {
     it('should return true for arrays with one or more values', () => {
-      assert.isTrue(FormatUtils.isValidQueryValue(['one']));
+      expect(FormatUtils.isValidQueryValue(['one'])).toBe(true);
     });
 
     it('should return false for empty arrays', () => {
-      assert.isFalse(FormatUtils.isValidQueryValue([]));
+      expect(FormatUtils.isValidQueryValue([])).toBe(false);
     });
 
     it('should return false for null values', () => {
-      assert.isFalse(FormatUtils.isValidQueryValue(null));
+      expect(FormatUtils.isValidQueryValue(null)).toBe(false);
     });
 
     it('should return false for undefined values', () => {
-      assert.isFalse(FormatUtils.isValidQueryValue(undefined));
+      expect(FormatUtils.isValidQueryValue(undefined)).toBe(false);
     });
 
     it('should return true for false', () => {
-      assert.isTrue(FormatUtils.isValidQueryValue(false));
+      expect(FormatUtils.isValidQueryValue(false)).toBe(true);
     });
 
     it('should return true for numeric 0', () => {
-      assert.isTrue(FormatUtils.isValidQueryValue(0));
+      expect(FormatUtils.isValidQueryValue(0)).toBe(true);
     });
   });
 
   describe('parseList', () => {
     describe('Strings', () => {
       it('splits a string on commas', () => {
-        var input = '"apple", "orange"';
-        var parsedList = FormatUtils.parseList(input);
-        assert.deepEqual(parsedList, ['apple', 'orange']);
+        const input = '"apple", "orange"';
+        const parsedList = FormatUtils.parseList(input);
+        expect(parsedList).toEqual(['apple', 'orange']);
       });
       it('respects internal commas and whitespace in a double quoted string', () => {
-        var input = '"apple, banana", "orange"';
-        var parsedList = FormatUtils.parseList(input);
-        assert.deepEqual(parsedList, ['apple, banana', 'orange']);
+        const input = '"apple, banana", "orange"';
+        const parsedList = FormatUtils.parseList(input);
+        expect(parsedList).toEqual(['apple, banana', 'orange']);
       });
       it('respects numbers wrapped in double quotes as strings', () => {
-        var input = '"1", "1.25"';
-        var parsedList = FormatUtils.parseList(input);
-        assert.deepEqual(parsedList, ['1', '1.25']);
+        const input = '"1", "1.25"';
+        const parsedList = FormatUtils.parseList(input);
+        expect(parsedList).toEqual(['1', '1.25']);
       });
       it('respects quoted strings', () => {
-        var input = '"\'to be or not to be\'", "that is the question"';
-        var parsedList = FormatUtils.parseList(input);
-        assert.deepEqual(parsedList, ['\'to be or not to be\'', 'that is the question']);
+        const input = '"\'to be or not to be\'", "that is the question"';
+        const parsedList = FormatUtils.parseList(input);
+        expect(parsedList).toEqual(['\'to be or not to be\'', 'that is the question']);
       });
     });
     describe('Numbers', () => {
       it('treats numbers wrapped in single quotes as numbers', () => {
-        var input = '\'1\', \'1.25\'';
-        var parsedList = FormatUtils.parseList(input);
-        assert.deepEqual(parsedList, [
+        const input = '\'1\', \'1.25\'';
+        const parsedList = FormatUtils.parseList(input);
+        expect(parsedList).toEqual([
           1, 1.25]);
       });
     });
     describe('Mixed', () => {
       it('allows for mixed types', () => {
-        var input = '"a, banana", \'1\', "1.25"';
-        var parsedList = FormatUtils.parseList(input);
-        assert.deepEqual(parsedList, ['a, banana', 1, '1.25']);
+        const input = '"a, banana", \'1\', "1.25"';
+        const parsedList = FormatUtils.parseList(input);
+        expect(parsedList).toEqual(['a, banana', 1, '1.25']);
       });
     });
     describe('Incorrect', () => {
       it('returns empty string for values that are not lists', () => {
-        var input = 'some name';
-        var parsedList = FormatUtils.parseList(input);
-        assert.deepEqual(parsedList, '');
+        const input = 'some name';
+        const parsedList = FormatUtils.parseList(input);
+        expect(parsedList).toEqual('');
       });
     });
   });
 
   describe('isList', () => {
     it('returns true for a string in expected list format', () => {
-      assert.isTrue(FormatUtils.isList("\"a thing\", '1', '56', \"another thing\""));
+      expect(FormatUtils.isList("\"a thing\", '1', '56', \"another thing\"")).toBe(true);
     });
     it('returns true for a string in expected list format with comma inside quotes', () => {
-      assert.isTrue(FormatUtils.isList("\"a thing, with another thing\", '1', '56', \"another thing\""));
+      expect(FormatUtils.isList("\"a thing, with another thing\", '1', '56', \"another thing\"")).toBe(true);
     });
     it('returns false for a string that is not in expected list format', () => {
-      assert.isFalse(FormatUtils.isList("a thing"));
+      expect(FormatUtils.isList("a thing")).toBe(false);
     });
   });
 
