9d4edd8701113131c52c15d9cd6e5fe85e2c3610
Adam Kasprowicz <adam.kasprowicz@keen.io>
2018-06-22 12:26:06 +0200

    fix autoreplace for css

--- replace-config.js
+++ replace-config.js
@@ -6,6 +6,12 @@ module.exports = {
     './node_modules/**',
     './replace-config.js'
   ],
-  from: RegExp(`${pkg.name}-(.*)\.bundle\.min\.js`, 'g'),
-  to: `${pkg.name}-${pkg.version}.bundle.min.js`,
+  from: [
+    RegExp(`${pkg.name}-(.*)\.bundle\.min\.js`, 'g'),
+    RegExp(`${pkg.name}-(.*)\.min\.css`, 'g')
+  ],
+  to: [
+    `${pkg.name}-${pkg.version}.bundle.min.js`,
+    `${pkg.name}-${pkg.version}.min.css`
+  ]
 }
