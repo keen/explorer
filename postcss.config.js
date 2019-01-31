68387ff5ed467949ee731d64f1881abfa262d007
Adam Kasprowicz <adam.kasprowicz@keen.io>
2019-01-31 11:59:43 +0100

    clean up, updates

--- postcss.config.js
+++ postcss.config.js
@@ -14,7 +14,7 @@ module.exports = {
         '>1%',
         'last 4 versions',
         'Firefox ESR',
-        'not ie < 9', // React doesn't support IE8 anyway
+        'not ie < 11', // React doesn't support IE8 anyway
       ],
       flexbox: 'no-2009',
     }),
