048a857bdcecac0a7b95b4cc78c4f79168b89e70
Artur Pryka <artur.pryka@keen.io>
2020-07-28 16:19:00 +0200

    feat: 🎸 radio-group component

---
+++ lib/js/app/queryCreator/components/RadioGroup/RadioGroup.test.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { render as rtlRender, fireEvent } from '@testing-library/react';
+
+import RadioGroup from './RadioGroup';
+
+const elements = [
+  {label: 'Radio 1', value: 'radio-1'},
+  {label: 'Radio 2', value: 'radio-2'},
+  {label: 'Radio 3', value: 'radio-3'},
+];
+
+const render = (overProps: any = {}) => {
+  const props = {
+    onChange: jest.fn(),
+    elements,
+    name: 'test',
+    ...overProps,
+  };
+
+  const wrapper = rtlRender(<RadioGroup {...props} />);
+
+  return {
+    props,
+    wrapper,
+  };
+};
+
+test('RadioGroup should be rendered', () => {
+  const {
+    wrapper: { getByTestId },
+  } = render();
+  const radioGroup = getByTestId('radio-group');
+
+  expect(radioGroup).toBeInTheDocument();
+});
+
+test('should render provided numner of radios', () => {
+  const {
+    wrapper: { getAllByRole },
+  } = render();
+  const inputs = getAllByRole('radio');
+
+  expect(inputs.length).toEqual(elements.length);
+});
+
+test('should call onChange', () => {
+  const {
+    wrapper: { getByTestId },
+    props,
+  } = render();
+  const radio = getByTestId('radio-1');
+  fireEvent.click(radio);
+
+  expect(props.onChange).toHaveBeenCalled();
+});
+
+test('should mark active radio', () => {
+  const {
+    wrapper: { getByTestId },
+  } = render({ value: 'radio-2'});
+  const radio = getByTestId('radio-2-active');
+
+  expect(radio).toBeInTheDocument();
+})
---
+++ lib/js/app/queryCreator/components/RadioGroup/RadioGroup.tsx
@@ -0,0 +1,62 @@
+import React, { FC, useCallback, useState } from 'react';
+import { Container, StyledLabel, Radio, StyledInput, Marker } from './RadioGroup.styles';
+
+type Group = {
+  label: string;
+  value: string;
+}
+
+type Props = {
+  /** Group name */
+  name: string;
+  /** Group elements */
+  elements: Group[];
+  /** Active element value */
+  value?: string;
+  /** onChange handler */
+  onChange: (e: React.ChangeEvent<HTMLInputElement>, value: string) => void;
+};
+const initialState = { x: 0, width: 0, height: 0 };
+
+const RadioGroup: FC<Props> = ({ name, elements, value, onChange }) => {
+  const [state, setState] = useState(initialState)
+
+  const activeRadioRef = useCallback(node => {
+    if (node !== null) {
+      const { x, width, height } = node.getBoundingClientRect();
+      setState({x, width, height });
+    }
+  }, []);
+
+  const markerMotion = {
+    initial: initialState,
+    animate: state,
+    exit: { opacity: 0 },
+    transition: { delay: 0.1 },
+  };
+
+  return (
+    <Container data-testid="radio-group">
+      {elements.map((radio) => (
+        <Radio key={radio.value} ref={radio.value === value ? activeRadioRef : null}>
+          <StyledInput
+            type="radio"
+            id={radio.value}
+            value={radio.value}
+            name={name} 
+            checked={radio.value === value}
+            onChange={e => onChange(e, radio.value)}
+          />
+          <StyledLabel
+            data-testid={`${radio.value}${radio.value === value ? '-active' : ''}`}
+            htmlFor={radio.value}>
+              {radio.label}
+            </StyledLabel>
+        </Radio>
+      ))}
+      {activeRadioRef && <Marker {...markerMotion } />}
+    </Container>
+  );
+};
+
+export default RadioGroup;
