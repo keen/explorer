1717b8034450791e0eff3483d30525ca989402d0
Artur Pryka <artur.pryka@keen.io>
2020-08-07 17:32:21 +0200

    test: 💍 update tests

--- lib/js/app/queryCreator/components/GroupBy/GroupBy.test.tsx
+++ lib/js/app/queryCreator/components/GroupBy/GroupBy.test.tsx
@@ -8,6 +8,7 @@ import {
 import configureStore from 'redux-mock-store';
 
 import GroupBy from './GroupBy';
+import text from './text.json';
 
 const render = (storeState: any = {}, overProps: any = {}) => {
   const mockStore = configureStore([]);
@@ -47,4 +48,49 @@ test('allows users to add group by settings', async () => {
   waitFor(() => {
     expect(store.getActions()).toMatchInlineSnapshot(`Array []`);
   });
+});
+
+test('should render exact number of properties with preserved order', async () => {
+  const storeState = {
+    query: {
+      groupBy: ['date', 'userId'],
+    },
+    events: {
+      schemas: {
+        purchases: { date: 'String', userId: 'String' },
+      },
+    },
+  };
+
+  const {
+    wrapper: { getAllByTestId },
+  } = render(storeState, { collection: 'purchases' });
+
+  const items = getAllByTestId('groupBy-property');
+
+  expect(items.length).toEqual(Object.keys(storeState.query.groupBy).length);
+
+  items.forEach((item, idx) => {
+    expect(item.textContent).toEqual(`${storeState.query.groupBy[idx]}×`);
+  });
+});
+
+test('should render title', () => {
+  const storeState = {
+    query: {
+      groupBy: undefined,
+    },
+    events: {
+      schemas: {
+        purchases: { date: 'String', userId: 'String' },
+      },
+    },
+  };
+
+  const {
+    wrapper: { getByText },
+  } = render(storeState, { collection: 'purchases' });
+
+  const title = getByText(text.title);
+  expect(title).toBeInTheDocument();
 });
