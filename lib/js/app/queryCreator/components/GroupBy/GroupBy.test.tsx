c4693f98476af940a6eef4b92a3879d4e4e54e62
Artur Pryka <artur.pryka@keen.io>
2020-08-07 14:27:47 +0200

    test: 💍 update tests

--- lib/js/app/queryCreator/components/GroupBy/GroupBy.test.tsx
+++ lib/js/app/queryCreator/components/GroupBy/GroupBy.test.tsx
@@ -1,78 +1,52 @@
-test('1', () => {
-  expect(1).toEqual(1);
+import React from 'react';
+import { Provider } from 'react-redux';
+import {
+  render as rtlRender,
+  fireEvent,
+  waitFor,
+} from '@testing-library/react';
+import configureStore from 'redux-mock-store';
+
+import GroupBy from './GroupBy';
+
+const render = (storeState: any = {}, overProps: any = {}) => {
+  const mockStore = configureStore([]);
+  const store = mockStore({ ...storeState });
+
+  const wrapper = rtlRender(
+    <Provider store={store}>
+      <GroupBy {...overProps} />
+    </Provider>
+  );
+
+  return {
+    store,
+    wrapper,
+  };
+};
+
+test('allows users to add group by settings', async () => {
+  const storeState = {
+    query: {
+      groupBy: undefined,
+    },
+    events: {
+      schemas: {
+        purchases: { date: 'String', userId: 'String' },
+      },
+    },
+  };
+
+  const {
+    wrapper: { getByTestId },
+    store,
+  } = render(storeState, { collection: 'purchases' });
+  const button = getByTestId('action-button');
+  fireEvent.click(button);
+
+  waitFor(() => {
+    expect(store.getActions()).toMatchInlineSnapshot(`Array []`);
+  });
 });
 
-// test('allows users to remove group by settings', () => {
-//   const storeState = {
-//     query: {
-//       groupBy: ['userId'],
-//     },
-//     events: {
-//       schemas: {
-//         purchases: { date: 'String', userId: 'String' },
-//       },
-//     },
-//   };
-//
-//   const {
-//     wrapper: { getByTestId },
-//     store,
-//   } = render(storeState, { collection: 'purchases' });
-//
-//   const button = getByTestId('remove-property');
-//   fireEvent.click(button);
-//
-//   expect(store.getActions()).toMatchInlineSnapshot(`
-//     Array [
-//       Object {
-//         "payload": Object {
-//           "groupBy": Array [
-//             "userId",
-//           ],
-//         },
-//         "type": "@query-creator/SET_GROUP_BY",
-//       },
-//       Object {
-//         "payload": Object {
-//           "groupBy": undefined,
-//         },
-//         "type": "@query-creator/SET_GROUP_BY",
-//       },
-//     ]
-//   `);
-// });
 
-// test('allows users to add group by settings', async () => {
-//   const storeState = {
-//     query: {
-//       groupBy: undefined,
-//     },
-//     events: {
-//       schemas: {
-//         purchases: { date: 'String', userId: 'String' },
-//       },
-//     },
-//   };
-//
-//   const {
-//     wrapper: { getByText, getByLabelText },
-//     store,
-//   } = render(storeState, { collection: 'purchases' });
-//   const button = getByText(text.addGroup);
-//   fireEvent.click(button);
-//
-//   await selectEvent.select(getByLabelText(text.label), 'userId');
-//
-//   waitFor(() => {
-//     expect(store.getActions()).toMatchInlineSnapshot(`
-//       Array [
-//         Object {
-//           "payload": Object {
-//             "groupBy": undefined,
-//           },
-//           "type": "@query-creator/SET_GROUP_BY",
-//         },
-//       ]
-//     `);
-//   });
-// });
