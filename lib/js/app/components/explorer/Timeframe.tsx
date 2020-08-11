9e56c332f40d5b3a913f7fa3c28742f58fc2504f
Artur Pryka <artur.pryka@keen.io>
2020-08-11 09:36:12 +0200

    refactor: 💡 remove comments

--- lib/js/app/components/explorer/Timeframe.tsx
+++ lib/js/app/components/explorer/Timeframe.tsx
@@ -34,10 +34,6 @@ const mapDispatchToProps = {
 
 const convertDateToString = (valueSelected, timezone) => {
   const value = valueSelected || moment(moment().format('YYYY-MM-DD'));
-  // const valueConverted = `${value.format('YYYY-MM-DD')}T${value.format(
-  //   'HH:mm'
-  // )}:00.000Z`;
-  // return valueConverted;
   return moment(value).tz(timezone).format();
 };
 
