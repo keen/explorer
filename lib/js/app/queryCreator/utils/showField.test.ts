fdb7d4ffbc2bbf942c64422e79f2a1e272420910
Artur Pryka <artur.pryka@keen.io>
2020-09-01 12:09:14 +0200

    test: 💍 update tests for showField

--- lib/js/app/queryCreator/utils/showField.test.ts
+++ lib/js/app/queryCreator/utils/showField.test.ts
@@ -1,39 +1,28 @@
 import { showField } from './showField';
+import { ANALYSIS_TYPES } from '../constants';
 
 test('should show analysisType for all types of analysis', () => {
-  const fieldCount = showField('analysisType', 'count');
-  const fieldSum = showField('analysisType', 'sum');
-  const fieldAverage = showField('analysisType', 'average');
-
-  expect(fieldCount).toBeTruthy();
-  expect(fieldSum).toBeTruthy();
-  expect(fieldAverage).toBeTruthy();
+  ANALYSIS_TYPES.forEach((type) => {
+    expect(showField('analysisType', type)).toBeTruthy();
+  });
 });
 
-test('should show percentile field only for percentile type of analysis', () => {
-  const fieldCount = showField('percentile', 'count');
-  const fieldSum = showField('percentile', 'sum');
-  const fieldAverage = showField('percentile', 'percentile');
-
-  expect(fieldCount).toBeFalsy();
-  expect(fieldSum).toBeFalsy();
-  expect(fieldAverage).toBeTruthy();
+test.each([
+  [showField('percentile', 'count'), 'count', false],
+  [showField('percentile', 'sum'), 'sum', false],
+  [showField('percentile', 'percentile'), 'percentile', true],
+])('showField for percentile field returns %s for %s', (a, _b, expected) => {
+  expect(a).toBe(expected);
 });
 
-test('should show only supported fields for extraction', () => {
-  const analysisType = showField('analysisType', 'extraction');
-  const eventCollection = showField('eventCollection', 'extraction');
-  const targetProperty = showField('targetProperty', 'extraction');
-  const email = showField('email', 'extraction');
-  const contentEncoding = showField('contentEncoding', 'extraction');
-  const propertyNames = showField('propertyNames', 'extraction');
-  const latest = showField('latest', 'extraction');
-
-  expect(analysisType).toBeTruthy();
-  expect(eventCollection).toBeTruthy();
-  expect(targetProperty).toBeFalsy();
-  expect(email).toBeTruthy();
-  expect(contentEncoding).toBeTruthy();
-  expect(propertyNames).toBeTruthy();
-  expect(latest).toBeTruthy();
+test.each([
+  [showField('analysisType', 'extraction'), 'analysisType', true],
+  [showField('eventCollection', 'extraction'), 'eventCollection', true],
+  [showField('targetProperty', 'extraction'), 'targetProperty', false],
+  [showField('email', 'extraction'), 'email', true],
+  [showField('contentEncoding', 'extraction'), 'contentEncoding', true],
+  [showField('propertyNames', 'extraction'), 'propertyNames', true],
+  [showField('latest', 'extraction'), 'latest', true],
+])('showField for extraction analysis returns %s for %s', (a, _b, expected) => {
+  expect(a).toBe(expected);
 });
