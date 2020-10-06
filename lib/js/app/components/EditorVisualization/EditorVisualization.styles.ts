c902fa43b6f60a36bfbfde2c54d6654a4c83d09c
Artur Pryka <artur.pryka@keen.io>
2020-10-06 08:57:55 +0200

    fix: 🐛 table in edit mode

--- lib/js/app/components/EditorVisualization/EditorVisualization.styles.ts
+++ lib/js/app/components/EditorVisualization/EditorVisualization.styles.ts
@@ -2,8 +2,10 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
+  min-width: 0;
 `;
 
 export const PickerContainer = styled.div`
   padding: 10px;
+  flex-shrink: 0;
 `;
--- lib/js/app/components/QueryVisualization/QueryVisualization.styles.ts
+++ lib/js/app/components/QueryVisualization/QueryVisualization.styles.ts
@@ -4,6 +4,8 @@ export const Container = styled.div`
   display: flex;
   width: 100%;
   flex-grow: 1;
+  padding: 10px;
+  box-sizing: border-box;
 `;
 
 export const JSONContainer = styled.div`
