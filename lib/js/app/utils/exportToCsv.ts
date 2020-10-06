9e54d755397b9552ba7366c6cf86296c1be27f8a
Artur Pryka <artur.pryka@keen.io>
2020-10-06 15:26:36 +0200

    refactor: 💡 remove unnecessary packages

--- lib/js/app/utils/exportToCsv.ts
+++ lib/js/app/utils/exportToCsv.ts
@@ -1,8 +1,44 @@
-import stringify from 'csv-stringify';
-
 import { download } from './download';
+import { parseQuery } from '@keen.io/parser';
 
 const FILENAME = 'chart';
+
+const convertToCSV = (
+  data = null,
+  columnDelimiter = ',',
+  lineDelimiter = '\n'
+) => {
+  let result: string;
+  let ctr: number;
+
+  if (data === null || !data.length) {
+    return null;
+  }
+
+  const keys = Object.keys(data[0]);
+
+  result = '';
+  result += keys.join(columnDelimiter);
+  result += lineDelimiter;
+
+  data.forEach((item: Record<string, any>) => {
+    ctr = 0;
+    keys.forEach((key) => {
+      if (ctr > 0) {
+        result += columnDelimiter;
+      }
+
+      result +=
+        typeof item[key] === 'string' && item[key].includes(columnDelimiter)
+          ? `"${item[key]}"`
+          : item[key];
+      ctr++;
+    });
+    result += lineDelimiter;
+  });
+
+  return result;
+};
 
 export const exportToCsv = ({
   data,
@@ -17,13 +53,9 @@ export const exportToCsv = ({
   if (!hasExtension) {
     downloadFileName = `${fileName}.csv`;
   }
-  console.log({ data }, downloadFileName);
 
-  stringify([data], { header: true }, (err, output) => {
-    if (err) {
-      console.log(err);
-    }
-    console.log('-----stringify, ', output);
-    download(output, downloadFileName, 'text/csv;encoding:utf-8');
-  });
+  const { result, steps } = data;
+  const parsedQuery = parseQuery({ result, steps });
+  const csvOutput = convertToCSV(parsedQuery.results);
+  download(csvOutput, downloadFileName, 'text/csv;encoding:utf-8');
 };
