5f7dd9c2914c6a339ce3eb941d41e42c3f688cbb
Maciej Rybaniec <maciej.rybaniec@gmail.com>
2020-10-28 10:13:53 +0100

    test: 💍 filter queries unit tests

--- lib/js/app/components/FilterQueries/FilterQueries.test.tsx
+++ lib/js/app/components/FilterQueries/FilterQueries.test.tsx
@@ -42,7 +42,7 @@ const render = (storeState: any = {}, overProps: any = {}) => {
   };
 };
 
-test('allows user to filter queries based on cache criteris', async () => {
+test('allows user to filter queries based on cache criteria', async () => {
   const {
     wrapper: { getByText },
     props,
@@ -55,6 +55,58 @@ test('allows user to filter queries based on cache criteris', async () => {
     const cacheFilter = getByText('browser_filters.show_only_cached_queries');
     fireEvent.click(cacheFilter);
 
-    expect(props.onUpdateCacheFilter).toHaveBeenLastCalledWith(true);
+    expect(props.onUpdateCacheFilter).toHaveBeenCalledWith(true);
   });
+});
+
+test('allows user to filter queries based on selected tags', async () => {
+  const {
+    wrapper: { getByText },
+    props,
+  } = render({
+    project: {
+      tagsPool: ['marketing', 'it'],
+    },
+  });
+
+  const element = getByText('browser_filters.title');
+  fireEvent.click(element);
+
+  await waitFor(() => {
+    const tagElement = getByText('marketing');
+    fireEvent.click(tagElement);
+
+    expect(props.onUpdateTagsFilters).toHaveBeenCalledWith(['marketing']);
+  });
+});
+
+test('allows user to clear filters', async () => {
+  const {
+    wrapper: { getByText },
+    props,
+  } = render();
+
+  const element = getByText('browser_filters.title');
+  fireEvent.click(element);
+
+  await waitFor(() => {
+    const element = getByText('browser_filters.clear');
+    fireEvent.click(element);
+
+    expect(props.onClearFilters).toHaveBeenCalled();
+  });
+});
+
+test('renders the number of active filters', async () => {
+  const {
+    wrapper: { getByText },
+  } = render(
+    {},
+    {
+      tagsFilters: ['marketing'],
+      showOnlyCachedQueries: true,
+    }
+  );
+
+  expect(getByText('browser_filters.title (2)')).toBeInTheDocument();
 });
