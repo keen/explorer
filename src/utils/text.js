412446c56014b031e2673463c52d6359406c9625
Artur Pryka <artur.pryka@keen.io>
2021-02-16 15:36:05 +0100

    refactor: 💡 remove obsolete methods

--- src/utils/text.js
+++ src/utils/text.js
@@ -1,12 +1,3 @@
-export const copyToClipboard = (str) => {
-  const el = document.createElement('textarea');
-  el.value = str;
-  document.body.appendChild(el);
-  el.select();
-  document.execCommand('copy');
-  document.body.removeChild(el);
-};
-
 export const slugify = (name) =>
   name
     .toLowerCase()
