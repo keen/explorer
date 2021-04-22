5496d0916cc969d256b12209411e2aac2baf2473
Artur Pryka <artur.pryka@keen.io>
2021-04-22 15:59:19 +0200

    style: 💄 add responsive styles for Query Creator

--- src/components/Editor/Editor.styles.ts
+++ src/components/Editor/Editor.styles.ts
@@ -5,18 +5,16 @@ import { BACKGROUND_MAIN } from '../../constants';
 
 export const NavBar = styled.div`
   margin-top: 15px;
-  border-bottom: solid 1px ${colors.gray[500]};
+  border-bottom: solid 1px ${colors.gray[300]};
 `;
 
 export const EditorActions = styled.div`
   display: flex;
-  padding: 0 20px 20px 20px;
-  background: ${BACKGROUND_MAIN};
+  padding: 10px 20px 20px 20px;
 `;
 
 export const SectionContainer = styled.div`
-  padding-bottom: 20px;
-  background: ${BACKGROUND_MAIN};
+  box-shadow: 0 2px 4px 0 ${transparentize(0.85, colors.black[500])};
 `;
 
 export const ActionButton = styled.div`
@@ -25,17 +23,14 @@ export const ActionButton = styled.div`
 
 export const CustomizationContainer = styled.div`
   padding: 15px 0;
-
   background: ${colors.white[500]};
-  border-bottom: solid 1px ${colors.gray[500]};
-`;
-
-export const QueryContainer = styled.div`
-  padding: 20px;
-  background: ${BACKGROUND_MAIN};
 `;
 
 export const Card = styled.div`
   background: ${colors.white[500]};
   box-shadow: 0 2px 4px 0 ${transparentize(0.85, colors.black[500])};
+`;
+
+export const Container = styled.div`
+  background: ${BACKGROUND_MAIN};
 `;
