a2763aec90995d005e93b43812f70fdad410cc59
Artur Pryka <artur.pryka@keen.io>
2021-08-24 15:24:51 +0200

    test: 💍 missing tests for reducer and sagas added

--- src/modules/app/appSaga.test.ts
+++ src/modules/app/appSaga.test.ts
@@ -1,14 +1,24 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 import sagaHelper from 'redux-saga-testing';
-import { getContext, put } from 'redux-saga/effects';
+import { getContext, put, select, take } from 'redux-saga/effects';
 import { NEW_QUERY_EVENT } from '@keen.io/query-creator';
 
-import { setViewMode, resetVisualization } from './actions';
-import { queriesActions } from '../queries';
-import { createNewQuery as createNewQueryFlow } from './appSaga';
+import {
+  setViewMode,
+  resetVisualization,
+  saveQuery,
+  showUpdateSavedQueryModal,
+} from './actions';
+import { getVisualization } from './selectors';
+import { getQuerySettings, queriesActions } from '../queries';
+import { savedQueryActions, savedQuerySelectors } from '../savedQuery';
+import {
+  createNewQuery as createNewQueryFlow,
+  saveQuery as saveQueryFlow,
+  saveExistingQuery as saveExistingQueryFlow,
+} from './appSaga';
 
 import { PUBSUB_CONTEXT } from '../../constants';
-import { savedQueryActions } from '../savedQuery';
 
 describe('createNewQuery()', () => {
   const test = sagaHelper(createNewQueryFlow());
@@ -38,5 +48,136 @@ describe('createNewQuery()', () => {
 
   test('reset saved query settings', (result) => {
     expect(result).toEqual(put(savedQueryActions.resetSavedQuery()));
+  });
+});
+
+describe('saveQuery()', () => {
+  const displayName = '@saved-query';
+  const refreshRate = 0;
+  const tags = [];
+  const name = 'saved-query';
+  const visualization = {
+    type: 'metric',
+    chartSettings: {},
+    widgetSettings: {},
+  };
+  const query = {};
+  const action = saveQuery(displayName, refreshRate, tags, name);
+  const test = sagaHelper(
+    saveQueryFlow(action as ReturnType<typeof saveQuery>)
+  );
+
+  test('selects query', (result) => {
+    expect(result).toEqual(select(getQuerySettings));
+    return query;
+  });
+
+  test('selects visualization', (result) => {
+    expect(result).toEqual(select(getVisualization));
+    return visualization;
+  });
+
+  test('saves query', (result) => {
+    const body = {
+      query,
+      metadata: {
+        displayName,
+        visualization,
+        tags,
+      },
+      refreshRate: refreshRate * 60 * 60,
+    };
+
+    expect(result).toEqual(put(queriesActions.saveQuery({ name, body })));
+  });
+});
+
+describe('saveExistingQuery()', () => {
+  const displayName = '@saved-query';
+  const refreshRate = 0;
+  const tags = [];
+  const name = 'saved-query';
+
+  describe('Scenario 1: Existing saved query is not used on any dashboards', () => {
+    const test = sagaHelper(saveExistingQueryFlow());
+    test('gets saved query', (result) => {
+      expect(result).toEqual(select(savedQuerySelectors.getSavedQuery));
+      return { displayName, refreshRate, tags, name };
+    });
+
+    test('gets dashboards connections', (result) => {
+      expect(result).toEqual(
+        put(savedQueryActions.getDashboardsConnection(name))
+      );
+    });
+
+    test('waits till dashboards connection get is done', (result) => {
+      expect(result).toEqual(
+        take(savedQueryActions.getDashboardsConnectionDone.type)
+      );
+    });
+
+    test('selects connected dashboards', (result) => {
+      expect(result).toEqual(
+        select(savedQuerySelectors.getConnectedDashboards)
+      );
+      return [];
+    });
+
+    test('selects connected dashboards error state', (result) => {
+      expect(result).toEqual(
+        select(savedQuerySelectors.getConnectedDashboardsError)
+      );
+      return false;
+    });
+
+    test('updates saved query', (result) => {
+      expect(result).toEqual(
+        put(saveQuery(displayName, refreshRate, tags, name))
+      );
+    });
+  });
+
+  describe('Scenario 2: Existing saved query is used on dashboards', () => {
+    const test = sagaHelper(saveExistingQueryFlow());
+    const dashboards = [
+      { title: '@dashboard-1', id: '@id-1' },
+      { title: '@dashboard-2', id: '@id-2' },
+      { title: '@dashboard-3', id: '@id-3' },
+    ];
+    test('gets saved query', (result) => {
+      expect(result).toEqual(select(savedQuerySelectors.getSavedQuery));
+      return { displayName, refreshRate, tags, name };
+    });
+
+    test('gets dashboards connections', (result) => {
+      expect(result).toEqual(
+        put(savedQueryActions.getDashboardsConnection(name))
+      );
+    });
+
+    test('waits till dashboards connection get is done', (result) => {
+      expect(result).toEqual(
+        take(savedQueryActions.getDashboardsConnectionDone.type)
+      );
+    });
+
+    test('selects connected dashboards', (result) => {
+      expect(result).toEqual(
+        select(savedQuerySelectors.getConnectedDashboards)
+      );
+      return dashboards;
+    });
+
+    test('selects connected dashboards error state', (result) => {
+      expect(result).toEqual(
+        select(savedQuerySelectors.getConnectedDashboardsError)
+      );
+      return false;
+    });
+
+    test('opens update saved query modal', (result) => {
+      expect(result).toEqual(put(showUpdateSavedQueryModal()));
+    });
   });
 });
