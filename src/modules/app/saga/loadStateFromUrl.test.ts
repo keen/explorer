382033ceb69de46b73f1a00e22920cae63fd1c7f
Artur Pryka <artur.pryka@keen.io>
2021-08-30 16:39:07 +0200

    test: 💍 update loadStateFromUrl scenario

--- src/modules/app/saga/loadStateFromUrl.test.ts
+++ src/modules/app/saga/loadStateFromUrl.test.ts
@@ -5,17 +5,88 @@ import { call, getContext, put, select, take } from 'redux-saga/effects';
 import { appActions } from '../index';
 import { savedQueryActions } from '../../savedQuery';
 import { getViewMode } from '../selectors';
+import { updateQueryCreator } from '../actions';
+import * as utils from '../utils';
 import { queriesActions } from '../../queries';
 import { loadStateFromUrl as loadStateFromUrlFlow } from './loadStateFromUrl';
 import { selectFirstSavedQuery } from './selectFirstSavedQuery';
 import { NOTIFICATION_MANAGER_CONTEXT } from '../../../constants';
 
 beforeEach(() => {
-  jest.resetModules();
+  jest.clearAllMocks();
+});
+
+jest.mock('../utils', () => {
+  return {
+    b64DecodeUnicode: jest.fn(),
+  };
 });
 
 describe('loadStateFromUrl()', () => {
-  describe('Scenario 1: Error when loading state from url in browser mode', () => {
+  describe('Scenario 1: State is successfully loaded from the url', () => {
+    const savedQuery = {
+      name: '@savedQuery',
+      displayName: 'Purchases',
+      tags: [],
+      isCloned: false,
+      cached: false,
+      refreshRate: 0,
+      exists: true,
+    };
+    const querySettings = {
+      analysis_type: 'funnel',
+      steps: [],
+      timezone: null,
+      timeframe: null,
+    };
+    const visualization = {
+      type: 'metric',
+      chartSettings: {},
+      widgetSettings: {},
+    };
+
+    jest.spyOn(utils, 'b64DecodeUnicode').mockReturnValueOnce(
+      JSON.stringify({
+        savedQuery,
+        query: querySettings,
+        visualization,
+      })
+    );
+
+    const test = sagaHelper(loadStateFromUrlFlow());
+
+    test('updates saved query', (result) => {
+      expect(result).toEqual(
+        put(savedQueryActions.updateSavedQuery(savedQuery))
+      );
+    });
+
+    test('sets visualization settings', (result) => {
+      expect(result).toEqual(
+        put(
+          appActions.setVisualization({
+            type: visualization.type,
+            chartSettings: visualization.chartSettings,
+            widgetSettings: visualization.widgetSettings,
+          })
+        )
+      );
+    });
+
+    test('sets editor view mode', (result) => {
+      expect(result).toEqual(put(appActions.setViewMode({ view: 'editor' })));
+    });
+
+    test('waits till query editor to be mounted', (result) => {
+      expect(result).toEqual(take(appActions.queryEditorMounted.type));
+    });
+
+    test('updates Query Creator', (result) => {
+      expect(result).toEqual(put(updateQueryCreator(querySettings)));
+    });
+  });
+
+  describe('Scenario 2: Error when loading state from url in browser mode', () => {
     const test = sagaHelper(loadStateFromUrlFlow());
 
     const notificationManager = {
@@ -56,7 +127,7 @@ describe('loadStateFromUrl()', () => {
     });
   });
 
-  describe('Scenario 2: Error when loading state from url in editor mode', () => {
+  describe('Scenario 3: Error when loading state from url in editor mode', () => {
     const test = sagaHelper(loadStateFromUrlFlow());
 
     const notificationManager = {
@@ -90,53 +161,6 @@ describe('loadStateFromUrl()', () => {
 
     test('resets saved query', (result) => {
       expect(result).toEqual(put(savedQueryActions.resetSavedQuery()));
-    });
-  });
-
-  describe.skip('Scenario 3: State is successfully loaded from the url', () => {
-    jest.doMock('../utils', () => {
-      return {
-        b64DecodeUnicode: () =>
-          '{"savedQuery":{"name":"@savedQuery","displayName":"Purchases","tags":[],"isCloned":false,"cached":false,"refreshRate":0,"exists":true},"query":{"analysis_type":"funnel","steps":[],"timezone":null,"timeframe":null},"visualization":"metric"}',
-      };
-    });
-
-    const savedQuery = {
-      name: '@savedQuery',
-      displayName: 'Purchases',
-      tags: [],
-      isCloned: false,
-      cached: false,
-      refreshRate: 0,
-      exists: true,
-    };
-    const querySettings = {
-      analysis_type: 'funnel',
-      steps: [],
-      timezone: null,
-      timeframe: null,
-    };
-    const visualization = 'metric';
-
-    // const notificationManager = {
-    //   showNotification: jest.fn(),
-    // };
-
-    jest
-      .spyOn(URLSearchParams.prototype, 'get')
-      .mockImplementation(
-        () =>
-          `?query=${JSON.stringify(querySettings)}&savedQuery=${JSON.stringify(
-            savedQuery
-          )}&visualization=${JSON.stringify(visualization)}`
-      );
-
-    const test = sagaHelper(loadStateFromUrlFlow());
-
-    test('updates saved query', (result) => {
-      expect(result).toEqual(
-        put(savedQueryActions.updateSavedQuery(savedQuery))
-      );
     });
   });
 });
